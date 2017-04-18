2eeaa05f03b173b70c0930801a64dbc1333876db
Parfait <jenny.filcheck@gmail.com>
2017-04-17 22:31:16 -0500

    solved first version of tree.js, still needs minor refactoring

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,24 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+
+  addChild(value) {
+    const tree = new Tree(value);
+    this.children.push(tree);
+  }
+
+  contains(value) {
+    let found = false;
+
+    const search = (children) => {
+      for (let i = 0, len = children.length; i < len; i++) {
+        if (children[i].value === value) found = true;
+        if (children[i].children.length > 0) search(children[i].children);
+      }
+    };
+    search(this.children);
+    return found;
+  }
 }
 
 module.exports = Tree;
