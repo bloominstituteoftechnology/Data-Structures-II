044d16b39b1e9c87287f40c2ed01d75b90bbce8e
manishavi <manishalal972@gmail.com>
2017-07-13 16:30:56 -0500

    passed three test

--- src/tree.js
+++ src/tree.js
@@ -4,19 +4,37 @@ class Tree {
     this.children = [];
   }
   // Should have the methods: `addChild`, and `contains`
-  addChild() {
+  addChild(val) {
     // When you add nodes to the `children` array
     // use `new Tree(value)` to create the node.
     // const node = new Tree();
-    const node = new Tree();
-    const value = this.value;
-    this.children = [{
-      value,
-      node }];
+    // const value = this.value;
+    const node = new Tree(val);
+    // children[0].value
+    this.children[this.children.length] = node;// = [{ value, node }];
   }
+  // node.addChild();
   contains() {
     return this;
   }
 }
 
 module.exports = Tree;
+
+// const tree = {
+//   value: 5,
+//   children: [
+//     {
+//       value: 10,
+//       children: []
+//     },
+//     {
+//       value: 12,
+//       children: []
+//     },
+//     {
+//       value: 2,
+//       children: []
+//     }
+//   ]
+// };
