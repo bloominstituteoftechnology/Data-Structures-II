1bd0b599099b5c6d9faf81b415b02a39eb2e0275
wlharvey4 <wlharvey4@mac.com>
2017-07-12 18:13:04 -0700

    tree.js working

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,24 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+
+  addChild(value) {
+    const child = new Tree(value);
+    this.children.push(child);
+  }
+
+  contains(value) {
+    const check = (i = 0) => {
+      if (i === this.children.length) return false;
+      if (value === this.children[i].value) return true;
+      if (this.children[i].children.length > 0) {
+        if (this.children[i].contains(value)) return true;
+      }
+      return check(++i);
+    };
+
+    return check();
+  }
 }
 
 module.exports = Tree;
