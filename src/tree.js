3c91fd688f9390939c06eb0fe0b21b21bafca08b
sfmajors373 <sfmajors373@gmail.com>
2017-07-13 16:03:01 +0000

    tree: beginning

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,17 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  root = null;
+  addChild(value) {
+    if (root === null) {
+      root = new Tree(value);
+    }
+  }
+
+  contains(value) {
+    let checkingTree =0;
+  }
+  
 }
 
 module.exports = Tree;
