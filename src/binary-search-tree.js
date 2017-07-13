c52f1728e0f84d11b3e094712e8b832598cfd3bb
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-13 17:24:22 -0400

    all tests passing for binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -4,55 +4,26 @@ class BinarySearchTree {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.storage = [value];
   }
   insert(newValue) {
-    // root varibale to represent root of
-    // tree and compare new values against
-    const treeRoot = this.value;
-    // node variable to insert new values
-    // and contiue w/ binary tree structure
+    this.storage.push(newValue);
     const node = new BinarySearchTree(newValue);
-    // if the newValue is <= the root value
-    if (newValue <= treeRoot) {
-      // and left node holds no value
-      if (!this.left) {
-        // value of left node becomes node
-        this.left = node;
-        // if there is a left node, call insert
-      } else {
-        // recursively on it with the new value
-        // to continue searching for empty node
-        this.left.insert(newValue);
-      } // if the newValue is > the root variable
-    } else if (newValue > treeRoot) {
-      // and right node holds no value
-      if (!this.right) {
-        // value of right becomes node
-        this.right = node;
-        // if there is a right node, call insert
-      } else {
-        // recursively on it with the new value
-        // to continue searching for empty node
-        this.right.insert(newValue);
-      }
+    if (newValue <= this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(newValue);
+    } else if (newValue > this.value) {
+      if (!this.right) this.right = node;
+      else this.right.insert(newValue);
     }
   }
-  contains() {
-    // should have a working "contains" method
-    // tree.insert(2);
-    // tree.insert(3);
-    // tree.insert(7);
-    // expect binarySearchTree.contains(7) to be => true
-    // expect binarySearchTree.contains(8) to be => false
+  contains(searchValue) {
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i] === searchValue) return true;
+    } return false;
   }
-  depthFirstForEach() {
-    // should execute a callback on every value in a tree using "depthFirstForEach"
-    // const array = [];
-    // const foo = value => ((array.push(value)));
-    // binarySearchTree.insert(2);
-    // binarySearchTree.insert(3);
-    // binarySearchTree.depthFirstForEach(foo);
-    // expect array to equal => [5, 2, 3]
+  depthFirstForEach(cb) {
+    return this.storage.forEach(i => cb(i));
   }
 }
 
