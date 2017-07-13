3d9c407f2dbb291bf8df2b54608adfde42481a30
manishavi <manishalal972@gmail.com>
2017-07-13 14:03:07 -0500

    whatever

--- src/tree.js
+++ src/tree.js
@@ -3,10 +3,16 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  // Should have the methods: `addChild`, and `contains`
   addChild() {
-    // value,
-    // children [ { value, children []}]
-    return this;
+    // When you add nodes to the `children` array
+    // use `new Tree(value)` to create the node.
+    // const node = new Tree();
+    const node = new Tree();
+    const value = this.value;
+    this.children = [{
+      value,
+      node }];
   }
   contains() {
     return this;
