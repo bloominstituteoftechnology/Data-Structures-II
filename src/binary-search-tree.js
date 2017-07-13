1b2908135d6c6d3da67a73a183458a90a8eb776b
wlharvey4 <wlharvey4@mac.com>
2017-07-13 06:48:40 -0700

    binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,42 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(value) {
+    const search = (n) => {
+      if (n.value >= value) {
+        if (n.left === null) {
+          return n.left = new BinarySearchTree(value);
+        }
+        return search(n.left);
+      }
+      if (n.right === null) {
+        return n.right = new BinarySearchTree(value);
+      }
+      return search(n.right);
+    };
+    return search(this);
+  }
+
+  contains(value) {
+    const search = (n) => {
+      if (n === null) return false;
+      if (n.value === value) return true;
+      return search(n.left) || search(n.right);
+    };
+    return search(this);
+  }
+
+  depthFirstForEach(cb) {
+    const loop = (n) => {
+      if (n === null) return;
+      cb(n.value);
+      loop(n.left);
+      loop(n.right);
+      return;
+    };
+    return loop(this);
+  }
 }
 
 module.exports = BinarySearchTree;
