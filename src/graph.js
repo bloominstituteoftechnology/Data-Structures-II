b973c5b05609760255b3101b18c5232e61bc4669
sfmajors373 <sfmajors373@gmail.com>
2017-07-14 21:20:29 +0000

    Graphs: some tests pass, last two don't

--- src/graph.js
+++ src/graph.js
@@ -1,17 +1,82 @@
 /* eslint-disable */
+class Node {
+  constructor(name) {
+    this.name = name;
+  }
+}
+class Edge {
+  constructor(source, destination) {
+    this.source = source;
+    this.destination = destination;
+  }
+}
 class Graph {
   constructor() {
     this.graph = {};
+    this.edges = [];
+    this.nodes = [];
   }
 
-  addNode(newNode, toNode) {}
+  addNode(newNode, toNode) {
     // if toNode given, should have edge between new and existing toNode
-  contains(value) {}
-  removeNode(value) {}
-  addEdge(fromNode, toNode) {}
-  getEdge(fromNode, toNode) {}
-  removeEdge(fromNode, toNode) {}
+    this.nodes.push(new Node(newNode));
+    if (this.nodes.length === 2 && this.edges.length === 0) {
+      this.edges.push(new Edge(this.nodes[0].name, this.nodes[1].name));
+    }
+    if (toNode) {
+      this.edges.push(new Edge(newNode, toNode));
+    }
+  }
+  contains(value) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      if (this.nodes[i].name === value){
+        return true;
+      }
+    }
+    return false;
+  }
+  removeNode(value) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      if (this.nodes[i].name === value) {
+        this.nodes.splice(i, 1);
+      }
+    }
+    for (let j = this.edges.length - 1; j >= 0; j--){
+      if (this.edges[j].source === value || this.edges[j].destination === value) {
+        this.edges.splice(j, 1);
+      }
+    }
+  }
+  addEdge(fromNode, toNode) {
+    this.edges.push(new Edge(fromNode, toNode));
+  }
+  getEdge(fromNode, toNode) {
+    for (let i = 0; i < this.edges.length; i++) {
+      if (this.edges[i].source === fromNode && this.edges[i].destination === toNode) {
+        return true;
+      }
+    }
+    return false;
+  }
+  removeEdge(fromNode, toNode) {
+    for (let i = 0; i < this.edges.length; i++) {
+      if (this.edges[i].source === fromNode && this.edges[i].destination === toNode)
+        delete this.edges[i];
+    }
+    for (let i = 0; i < this.nodes.length; i++) {
+      if (this.nodes[i].name === fromNode || this.nodes[i].name ===toNode){
+        if(this.nodes[i].edges.length === 0) {
+          this.nodes.splice(i, 1);
+	}
+      }
+    }
+    console.log(this.nodes);
+  }
 }
+
+const agraph = new Graph();
+agraph.addNode('pineapple');
+agraph.addNode('banana');
 
 module.exports = Graph;
 
