d05eb7218fcdb810ae90eff70464585928633401
manishavi <manishalal972@gmail.com>
2017-07-14 14:06:26 -0500

    constructor array

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,12 +2,14 @@
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
-    this.left = null;
+    this.left  = null;
     this.right = null;
+    this.arr   = [];
   }
   // Should have the methods: `insert`, `contains`, and `depthFirstForEach`
-  insert() {
-    return this;
+  insert(value) {
+    const node = new BinarySearchTree;
+    this.left = value;
   }
   contains() {
     return this;
@@ -18,10 +20,15 @@ class BinarySearchTree {
 }
 
 const binTest = new BinarySearchTree;
-console.log(`binTest is a instance of class BinarySearchTree: ${binTest}`);
+// console.log(`binTest is a instance of class BinarySearchTree: ${binTest}`);
+console.log(binTest);
+// insert()
+console.log(typeof binTest.insert()); // <-- function
 console.log("The method binTest.insert() results in: ", binTest.insert());
-console.log("The method binTest.contains() results in:", binTest.contains());
-console.log("The method binTest.depthFirstForEach() results in:", binTest.depthFirstForEach());
+console.log("The method binTest.insert() results in: ", binTest.insert('Anshuman'));
+
+// console.log("The method binTest.contains() results in:", binTest.contains());
+// console.log("The method binTest.depthFirstForEach() results in:", binTest.depthFirstForEach());
 
 
 module.exports = BinarySearchTree;
