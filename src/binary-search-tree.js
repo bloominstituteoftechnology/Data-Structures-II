3eb08b63db85e2077d5134ca7743e1df2226a021
manishavi <manishalal972@gmail.com>
2017-07-14 19:12:20 -0500

    BST passes first two tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,16 +2,18 @@
 class BinarySearchTree {
   constructor(parameter) { // <-- parameter is the function PARAMETER
     this.value = parameter;
-    this.left  = null;
+    this.left = null;
     this.right = null;
-    // this.arr   = parameter;
   }
   insert(val) {
-    // should insert values at the correct location in the tree
     const node = new BinarySearchTree(val);
-    // if ((this.value === undefined) && (this.left > val)) {
-    if (val <= this.value) return this.left = val;
-    return this.right = val;
+    if (val <= this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(val);
+    } else if (val > this.value) {
+      if (!this.right) this.right = node;
+      else this.right.insert(val);
+    }
   }
   contains() {
     return this;
@@ -21,21 +23,15 @@ class BinarySearchTree {
   }
 }
 
-const binTest = new BinarySearchTree(5550); // 'abc' is the ARGUMENT we pass in to the function.
-// const binTest = new BinarySearchTree;
-// console.log(`binTest is a instance of class BinarySearchTree: ${binTest}`);
-console.log(binTest);
-// insert()
-// console.log(typeof binTest.insert()); // <-- function
-// console.log("The method binTest.insert() results in: ", binTest.insert());
-// console.log("The method binTest.insert() results in: ", binTest.insert('Anshuman'));
-console.log(binTest.insert(72000));
-console.log(binTest);
-console.log(binTest.insert(1));
-console.log(binTest);
-
-// console.log("The method binTest.contains() results in:", binTest.contains());
-// console.log("The method binTest.depthFirstForEach() results in:", binTest.depthFirstForEach());
-
+// const binTest = new BinarySearchTree(5); // '5' is the ARGUMENT we pass in to the function.
+// console.log(binTest);
+// binTest.insert(2);
+// console.log(binTest);
+// binTest.insert(3);
+// console.log(binTest);
+// binTest.insert(7);
+// console.log(binTest);
+// binTest.insert(6);
+// console.log(binTest);
 
 module.exports = BinarySearchTree;
