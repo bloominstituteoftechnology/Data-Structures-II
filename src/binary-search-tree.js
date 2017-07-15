42b9a2cc62ddba2e9038d1d43fd108ab42401798
mixelpixel <mixelpix@mac.com>
2017-07-14 20:23:13 -0400

    insert()

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,15 +1,21 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 class BinarySearchTree {
-  constructor() { // <-- parameter is the function PARAMETER
-    this.value = [];
-    this.left  = null;
+  constructor(parameter) { // <-- parameter is the function PARAMETER
+    this.value = parameter;
+    this.left = null;
     this.right = null;
-    // this.arr   = parameter;
+    // this.anArray = [parameter];
   }
-  insert(val) {
-    // should insert values at the correct location in the tree
-    const node = new BinarySearchTree;
-    this.value = node;
+  insert(something) {
+    // this.anArray.push(something);
+    const node = new BinarySearchTree(something);
+    if (something <= this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(something);
+    } else if (something > this.value) {
+      if (!this.left) this.right = node;
+      else this.right.insert(something);
+    }
   }
   contains() {
     return this;
@@ -19,16 +25,21 @@ class BinarySearchTree {
   }
 }
 
-const binTest = new BinarySearchTree('abc'); // 'abc' is the ARGUMENT we pass in to the function.
-// console.log(`binTest is a instance of class BinarySearchTree: ${binTest}`);
-console.log(binTest);
-// insert()
-console.log(typeof binTest.insert()); // <-- function
-console.log("The method binTest.insert() results in: ", binTest.insert());
-console.log("The method binTest.insert() results in: ", binTest.insert('Anshuman'));
+// class BinarySearchTree
+const binTest = new BinarySearchTree(555); // 'abc' is the ARGUMENT we pass in to the function.
+console.log(`1. binTest is a instance of class BinarySearchTree: ${binTest}`);
+console.log('2.', binTest);
 
-// console.log("The method binTest.contains() results in:", binTest.contains());
-// console.log("The method binTest.depthFirstForEach() results in:", binTest.depthFirstForEach());
+// insert()
+console.log(`3. ${binTest.insert instanceof Function}`);
+console.log(`4. The method binTest.insert() results in: ${binTest.insert()}`);
+console.log(`5. The method binTest.insert(1234) results in: ${binTest.insert(1234)}`);
+// what's in our Binary Tree Seach object?
+console.log('6. Invoking binTest.this results in:', binTest.contains());
+
+
+// console.log('The method binTest.contains() results in:', binTest.contains());
+// console.log('The method binTest.depthFirstForEach() results in:', binTest.depthFirstForEach());
 
 
 module.exports = BinarySearchTree;
