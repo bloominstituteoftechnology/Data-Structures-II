c25622d73d1c0105cff8f94e1017fb6f837f82bb
sunjieming <ben@lambdaschool.com>
2017-07-17 09:50:51 -0700

    Added solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,50 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(value) {
+    const newNode = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = newNode;
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (!this.right) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  contains(target) {
+    if (this.value === target){
+      return true;
+    }
+    if (this.left) {
+      if (this.left.contains(target)) {
+        return true;
+      }
+    }
+    if (this.right){
+      if (this.right.contains(target)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  depthFirstLog(cb) {
+    cb (this.value);
+    if (this.right){
+      this.right.depthFirstLog(cb);
+    }
+    if (this.left){
+      this.left.depthFirstLog(cb);
+    }
+  }
 }
 
 module.exports = BinarySearchTree;
--- src/graph.js
+++ src/graph.js
@@ -3,6 +3,57 @@ class Graph {
   constructor() {
     this.graph = {};
   }
+  addNode(newNode, toNode) {
+    this.graph[newNode] = {};
+    this.graph[newNode].edges = [];
+    if (Object.keys(this.graph).length === 2 && !toNode) {
+      Object.keys(this.graph).forEach((key) => {
+        const next = this.graph[key];
+        if (key !== newNode) toNode = key;
+      });
+    }
+    if (toNode) {
+      this.graph[newNode].edges.push(toNode);
+      this.graph[toNode].edges.push(newNode);
+    }
+  }
+  contains(node) {
+    let hasNode = false;
+    Object.keys(this.graph).forEach(key => {
+      if (key === node) hasNode = true;
+    });
+    return hasNode;
+  }
+  removeNode(node) {
+    delete this.graph[node];
+    Object.keys(this.graph).forEach((key) => {
+      const index = obj[key].edges.indexOf(node);
+      if (index === -1) return;
+      obj[key].edges.splice(index, 1);
+    });
+  }
+
+  getEdge(fromNode, toNode) {
+    const fromNodeHasEdge = this.graph[fromNode].edges.indexOf(toNode) !== -1;
+    const toNodeHasEdge = this.graph[toNode].edges.indexOf(fromNode) !== -1;
+    return fromNodeHasEdge && toNodeHasEdge;
+  }
+
+  addEdge(fromNode, toNode) {
+    const fromHasntEdge = this.graph[fromNode].edges.indexOf(toNode) === -1;
+    const toHasntEdge = this.graph[fromNode].edges.indexOf(toNode) === -1;
+    if (fromHasntEdge) this.graph[fromNode].edges.push(toNode);
+    if (toHasntEdge) this.graph[toNode].edges.push(fromNode);
+  }
+
+  removeEdge(fromNode, toNode) {
+    const indexOfFrom = this.graph[fromNode].edges.indexOf(toNode);
+    const indexOfTo = this.graph[toNode].edges.indexOf(fromNode);
+    this.graph[fromNode].edges.splice(indexOfFrom, 1);
+    this.graph[toNode].edges.splice(indexOfTo, 1);
+    if (this.graph[fromNode].edges.length === 0) delete this.graph[fromNode];
+    if (this.graph[toNode].edges.length === 0) delete this.graph[toNode];
+  }
 }
 
 module.exports = Graph;
