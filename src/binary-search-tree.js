6a74021c3ed75215cdad95bafd35c2af7fc3146b
mixelpixel <mixelpix@mac.com>
2017-07-17 02:37:37 -0400

    depthFirstSearch console test suite

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -38,39 +38,50 @@ class BinarySearchTree { // BST is invoked with a given argument, e.g. `const x 
     }
   }
   // `depthFirstForEach(cb)` should iterate over the tree using DFS and passes each node of the tree to the given callback function.
-  depthFirstForEach() {
-    return this;
+  depthFirstForEach(cb) {
+    this.value = cb(this.value);        // PASSES INITIAL VALUE TO CALLBACK FUNCTION
+    // whatever the value, if it is true that there's a right value
+    if (this.right) {
+      this.right.depthFirstForEach(cb); // PASSES RIGHT VALUES TO CALLBACK FUNCTION
+    // whatever the value, if it is true that there's a left value
+    } else if (this.left) {
+      this.left.depthFirstForEach(cb);  // PASSES LEFT VALUES TO CALLBACK FUNCTION
+    }
   }
 }
 
-// // TEST SUITE
-// // class BinarySearchTree
-// const binTest = new BinarySearchTree(555); // 'abc' is the ARGUMENT we pass in to the function.
-// console.log('const binTest = new BinarySearchTree(555).');
-// console.log(`1. binTest is a instance of class BinarySearchTree: ${binTest}.`);
-// console.log('2. binTest is:', binTest);
-// console.log(`3. binTest is: ${binTest}.`); // <--- WHY THE PRINT DIFF????????????????????????
-// console.log(`4. const binTest = new BinarySearchTree(555) gave us a binTest.value of: ${binTest.value}.\n`);
-//
-// // insert()
-// console.log('insert()');
-// console.log(`1. Is binTest.insert a function? ${binTest.insert instanceof Function}.`);
-// console.log(`2. The method binTest.insert() returns: ${binTest.insert()} - it just changes the STATE of itself.`);
-// console.log(`3. The method binTest.insert(1234) returns: ${binTest.insert(111)} - it just changes the STATE of itself.\n`);
-// binTest.insert(222);
-// binTest.insert(1000);
-//
-// // contains()
-// console.log('contains()');
-// console.log(`1. The method binTest.contains() results in a ${binTest.contains()}`);
-// console.log('2. Q: Is 111  in binTest? A:', binTest.contains(111));  // <--- true
-// console.log('2. Q: Is 222  in binTest? A:', binTest.contains(222));  // <--- true
-// console.log('2. Q: Is 555  in binTest? A:', binTest.contains(555));  // <--- true
-// console.log('2. Q: Is 1000 in binTest? A:', binTest.contains(1000)); // <--- true
-// console.log('3. Q: Is 2000 in binTest? A:', binTest.contains(2000)); // <--- false
-//
-// // // depthFirstForEach()
-// console.log('depthFirstForEach()');
-// console.log('1. The method binTest.depthFirstForEach() results in:', binTest.depthFirstForEach());
+// TEST SUITE
+// class BinarySearchTree
+const binTest = new BinarySearchTree(555); // 'abc' is the ARGUMENT we pass in to the function.
+console.log('const binTest = new BinarySearchTree(555).');
+console.log(`1. binTest is a instance of class BinarySearchTree: ${binTest}.`);
+console.log('2. binTest is:', binTest);
+console.log(`3. binTest is: ${binTest}.`); // <--- WHY THE PRINT DIFF????????????????????????
+console.log(`4. const binTest = new BinarySearchTree(555) gave us a binTest.value of: ${binTest.value}.\n`);
+
+// insert()
+console.log('insert()');
+console.log(`1. Is binTest.insert a function? ${binTest.insert instanceof Function}.`);
+console.log(`2. The method binTest.insert() returns: ${binTest.insert()} - it just changes the STATE of itself.`);
+console.log(`3. The method binTest.insert(1234) returns: ${binTest.insert(111)} - it just changes the STATE of itself.\n`);
+binTest.insert(222);
+binTest.insert(1000);
+
+// contains()
+console.log('contains()');
+console.log(`1. The method binTest.contains() results in a ${binTest.contains()}`);
+console.log('2. Q: Is 111  in binTest? A:', binTest.contains(111));  // <--- true
+console.log('2. Q: Is 222  in binTest? A:', binTest.contains(222));  // <--- true
+console.log('2. Q: Is 555  in binTest? A:', binTest.contains(555));  // <--- true
+console.log('2. Q: Is 1000 in binTest? A:', binTest.contains(1000)); // <--- true
+console.log('3. Q: Is 2000 in binTest? A:', binTest.contains(2000)); // <--- false
+
+// // depthFirstForEach()
+console.log('\ndepthFirstForEach()');
+console.log('1. binTest BEFORE Callback() invoked:\n', binTest);
+const foo = x => x * 2;
+console.log(`2. the callback function: ${foo}`);
+console.log('3. The method binTest.depthFirstForEach(foo) results in:', binTest.depthFirstForEach(foo));
+console.log('4. binTest AFTER Callback() invoked:\n', binTest);
 
 module.exports = BinarySearchTree;
