46293beb508839f1a490bde8007e039e73a4e519
manishavi <manishalal972@gmail.com>
2017-07-17 14:51:35 -0500

    passed first test for graph

--- src/graph.js
+++ src/graph.js
@@ -3,6 +3,17 @@ class Graph {
   constructor() {
     this.graph = {};
   }
+  addNode(newNode, toNode) {
+    this.graph.push({
+    newNode,
+    toNode
+    });
+  }
+  contains(value) {}
+  removeNode(value) {}
+  addEdge(fromNode, toNode) {}
+  getEdge(fromNode, toNode) {}
+  removeEdge(fromNode, toNode) {}
 }
 
 module.exports = Graph;
