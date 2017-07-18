bce9c9f9c1a5417d9e816da13be572da82dcff3d
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-18 20:53:40 +0000

    Binary-search-tree completed

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,9 +1,77 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
+// class BinarySearchTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//     this.container = [value];
+//   }
+
+//   insert(value) {
+//     const node = new BinarySearchTree(value);
+//     if (value <= this.value) {
+//       if (!this.left) this.left = node;
+//       else this.left.insert(value);
+//     } else if (value > this.value) {
+//       if (!this.right) this.right = node;
+//       else this.right.insert(value);
+//     }
+//   }
+
+//   contains(value) {
+//     if (value === this.value) {
+//       return true;
+//     } else if (value > this.value) {
+//       if (this.right) {
+//         return this.right.contains(value);
+//       }
+//       return false;
+//     }
+//   }
+
+//   depthFirstForEach(cb) {
+//     return this.container.forEach((i) => {
+//       cb(i);
+//     });
+//   }
+// }
+
+
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.container = [value];
+  }
+
+  insert(value) {
+    this.container.push(value);
+    const node = new BinarySearchTree(value);
+    if (value <= this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(value);
+    } else if (value > this.value) {
+      if (!this.right) this.right = node;
+      else this.right.insert(value);
+    }
+  }
+
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    } else if (value > this.value) {
+      if (this.right) {
+        return this.right.contains(value);
+      }
+      return false;
+    }
+  }
+
+  depthFirstForEach(cb) {
+    return this.container.forEach((i) => {
+      cb(i);
+    });
   }
 }
 
