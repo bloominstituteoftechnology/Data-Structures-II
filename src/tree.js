720416f5a54b3973c3f49184af08614fda5bdb01
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-18 01:51:39 +0000

    Passed all tests for 'tree.js'

--- src/tree.js
+++ src/tree.js
@@ -2,7 +2,21 @@ class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
+    this.parent = null;
+  }
+  addChild(value) {
+    return this.children.push(new Tree(value));
+  }
+  contains(value) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(value)) return true;
+    }
+    if (this.value !== value) {
+      return false;
+    }
+    return true;
   }
 }
+
 
 module.exports = Tree;
