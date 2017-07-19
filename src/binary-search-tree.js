6f1c90985eed4bcd91b07885d262aa5f219a581f
Chooster <jcooleycoder@gmail.com>
2017-07-18 19:01:52 -0700

    Updated

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -4,19 +4,7 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
-<<<<<<< HEAD
   insert(value) {
-=======
-//   root(value) {
-//     this.root = value;
-//   }
-  insert(value) {
-//     let tempRoot = this;
-//     if (!tempRoot) {
-//       tempRoot = new BinarySearchTree(value);
-//       return;
-//     }
->>>>>>> 6287670a445200ab2366a97ec0edc382c9bb30ba
     let currentNode = this;
     const newNode = new BinarySearchTree(value);
     while (currentNode) {
@@ -36,7 +24,6 @@ class BinarySearchTree {
     }
   }
   contains(value) {
-<<<<<<< HEAD
     let bool = false;
     const search = (node) => {
       if (value === node.value) bool = true;
@@ -48,26 +35,8 @@ class BinarySearchTree {
   }
   depthFirstForEach(cb, node = this) {
     cb(node.value);
-    if (node.left) this.depthFirstForEach(cb, node.left);
-    if (node.right) this.depthFirstForEach(cb, node.right);
-=======
-    let currentNode = this;
-    while (currentNode) {
-      if (value < currentNode.value) {
-        if (value === currentNode.value) return true;
-        currentNode = currentNode.left;
-      } else {
-        if (value === currentNode.value) return true;
-        currentNode = currentNode.right;
-      }
-    }
-    return false;
-  }
-  depthFirstForEach(cb, node = this) {
-    cb(node.value);
     if (node.right) this.depthFirstForEach(cb, node.right);
     if (node.left) this.depthFirstForEach(cb, node.left);
->>>>>>> 6287670a445200ab2366a97ec0edc382c9bb30ba
   }
 }
 module.exports = BinarySearchTree;
