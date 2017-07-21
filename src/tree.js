6d44d10f9849ddd8c12d56af9ff564c55c51cb3c
gunghogirl <gunghogirl@gmail.com>
2017-07-20 20:46:47 -0700

    upto contains

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,22 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+
+  addChild(value) {
+     const node = {
+       value: value,
+       children: [],
+     };
+     this.children.push(node);
+  }
+
+  contains(value) {
+    if (this.value === value) return true;
+    for (let i = 0; i < this.children; i++) {
+      if (this.children[i].value !== value) this.contains.value;
+    }
+    return false;
+  }
 }
 
 module.exports = Tree;
