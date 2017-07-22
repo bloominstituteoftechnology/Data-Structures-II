5e95882d556676f92201dd90a5ac80fb3e2e8306
satishvattikuti <satishvattikuti94@gmail.com>
2017-07-21 21:54:09 -0400

    done with trees

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,32 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+  insert(val) {
+    if (val < this.value && this.left === null) {
+      this.left = new BinarySearchTree(val);
+    } else if (val < this.value && this.left !== null) {
+      this.left.insert(val);
+    } else if (val > this.value && this.right === null) {
+      this.right = new BinarySearchTree(val);
+    } else if (val > this.value && this.right !== null) {
+      this.right.insert(val);
+    }
+  }
+  contains(val) {
+    if (val === null) {
+      return false;
+    } else if (val === this.value) {
+      return true;
+    } else if (val > this.value) {
+      return this.right.contains(val);
+    } else if (val < this.value) {
+      return this.left.contains(val);
+    }
+    return false;
+  }
+  depthFirstForEach() {
+
+  }
 }
 
 module.exports = BinarySearchTree;
