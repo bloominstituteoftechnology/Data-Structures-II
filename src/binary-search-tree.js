7363653f7065ef8ac07dcc4f31f5da1c9ee8e899
kiyoshi626 <travis.nakano@gmail.com>
2017-07-22 17:56:51 -0600

    udated tree exercises and completed half of graph exercise

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,49 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+  insert(value) {
+    const node = new BinarySearchTree(value);
+    function recurse(bst) {
+      if (bst.value > value && bst.left === null) {
+        bst.left = node;
+      } else if (bst.value > value) {
+        recurse(bst.left);
+      } else if (bst.value < value && bst.right === null) {
+        bst.right = node;
+      } else if (bst.value < value) {
+        recurse(bst.right);
+      }
+    }
+    recurse(this);
+  }
+
+  contains(value) {
+    let doesContain = false;
+    function recurse(bst) {
+      if (bst.value === value) {
+        doesContain = true;
+      } else if (bst.left !== null && value < bst.value) {
+        recurse(bst.left);
+      } else if (bst.right !== null && value > bst.value) {
+        recurse(bst.right);
+      }
+    }
+    recurse(this);
+    return doesContain;
+  }
+
+  depthFirstForEach(callback) {
+    function recurse(bst) {
+      callback.call(bst, bst.value);
+      if (bst.left !== null) {
+        recurse(bst.left);
+      }
+      if (bst.right !== null) {
+        recurse(bst.right);
+      }
+    }
+    recurse(this);
+  }
 }
 
 module.exports = BinarySearchTree;
--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,22 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  addChild(value) {
+    const newNode = new Tree(value);
+    this.children.push(newNode);
+  }
+  contains(target) {
+    let result = false;
+    const inner = (obj) => {
+      if (obj.value === target) {
+        result = true;
+      }
+      obj.children.forEach((child) => {
+        inner(child);
+      });
+    };
+    inner(this);
+    return result;
+  }
 }
-
 module.exports = Tree;
