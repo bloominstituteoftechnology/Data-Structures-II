6ada86aea50aeb91e1ad335f23b23062f3c262ad
Frank Faustino <frank@Franks-MacBook-Pro.local>
2017-08-17 13:10:59 -0700

    Completed tree.js

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,22 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  addChild(value) {
+    this.children.push(new Tree(value));
+  }
+
+  contains(value) {
+    let flag = false;
+    const search = (children) => {
+      for (let i = 0, len = children.length; i < len; i++) {
+        if (children[i].value === value) flag = true;
+        if (children[i].children.length > 0) search(children[i].children);
+      }
+    };
+    search(this.children);
+    return flag;
+  }
 }
+
 
 module.exports = Tree;
