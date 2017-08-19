938f3c9fa1faf836e506b6d0164987d6c0cf40fd
phytertek <ryan.phytertek@gmail.com>
2017-08-18 21:53:41 -0400

    Starting over on Heap to create a MaxHeap

--- src/heap.js
+++ src/heap.js
@@ -1,53 +1,33 @@
-class Node {
-  constructor(val, priority) {
-    this.value = val;
-    this.priority = priority;
-    this.next = null;
-  }
-}
 
-class PriorityQueue {
+// 'Parent', Math.floor(n/2) - Math.round(n/2) === -1 ? Math.floor(n/2) : Math.floor(n/2) - 1
+// 'Children', Math.floor(n * 2) + 1 &&  Math.floor(n * 2) + 2
+class MaxHeap {
   constructor() {
-    this.first = null;
-    this.heap = null;
+    this.storage = [];
   }
 
-  insert(value, priority) {
-    const newNode = new Node(value, priority);
-    this.heap.push(newNode);
-    let currentNodeIdx = this.heap.length - 1;
-    let currentNodeParentIdx = Math.floor(currentNodeIdx / 2);
-    while (
-      this.heap[currentNodeParentIdx] &&
-      newNode.priority > this.heap[currentNodeParentIdx].priority
-    ) {
-      const parent = this.heap[currentNodeParentIdx];
-      this.heap[currentNodeParentIdx] = newNode;
-      this.heap[currentNodeIdx] = parent;
-      currentNodeIdx = currentNodeParentIdx;
-      currentNodeParentIdx = Math.floor(currentNodeIdx / 2);
-    }
+  insert(value) {
+    
   }
 
-  remove() {
-    if (this.heap.length < 3) {
-      const toReturn = this.heap.pop();
-      this.heap[0] = null;
-      return toReturn;
-    }
-    const toRemove = this.heap[1];
-    this.heap[1] = this.heap.pop();
-    const currentIdx = 1;
-    const [left, right] = [2 * currentIdx, (2 * currentIdx) + 1];
-    const currentChildIdx = this.heap[right] && this.heap[right].priority >= this.heap[left].priority ? right : left;
-    while (this.heap[currentChildIdx] && this.heap[currentIdx].priority <= this.heap[currentChildIdx].priority) {
-      const currentNode = this.heap[currentIdx]
-      const currentChildNode = this.heap[currentChildIdx];
-      this.heap[currentChildIdx] = currentNode;
-      this.heap[currentIdx] = currentChildNode;
-    }
-    return toRemove;
+  delete() {
+    
   }
+
+  getMax() {
+    return this.storage[0];
+  }
+
+  get size() {
+    return this.storage.length;
+  }
+
+  bubbleUp(index) {
+    
+  }
+
+  siftDown(index) {
+    
+  }
+  
 }
-
-module.exports(PriorityQueue);
