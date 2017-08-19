f1278b2f75967355656d4499f09467857e3b0994
Dylan <dylan@Dylans-MacBook.local>
2017-08-18 21:14:41 -0400

    updates

--- src/heap.js
+++ src/heap.js
@@ -4,6 +4,20 @@ class Heap {
   }
   insert(value) {
     this.heap.push(value);
+    this.sort();
+  }
+  delete() {
+    this.heap[1] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.sort();
+  }
+  getMax() {
+    return this.heap[1];
+  }
+  getSize() {
+    return this.heap.length - 1;
+  }
+  sort() {
     if (this.heap.length > 2) {
       const rec = (len) => {
         if (len === 1) return;
@@ -19,32 +33,6 @@ class Heap {
       rec(this.heap.length - 1);
     }
   }
-  removeLargest() {
-    this.heap[1] = this.heap[this.heap.length - 1];
-    this.heap.pop();
-    if (this.heap.length > 2) {
-      const rec = (start) => {
-        if ((start * 2) > this.heap.length) return;
-        if (this.heap[start] < this.heap[start * 2] && (this.heap[start * 2] > this.heap[(start * 2) + 1] || !this.heap[(start * 2) + 1])) {
-          const lower = this.heap[start];
-          const higher = this.heap[start * 2];
-          this.heap[start * 2] = lower;
-          this.heap[start] = higher;
-          rec(start * 2);
-        }
-        if (this.heap[start] < this.heap[(start * 2) + 1] && (this.heap[start * 2] < this.heap[(start * 2) + 1] || !this.heap[start * 2])) {
-          const lower = this.heap[start];
-          const higher = this.heap[(start * 2) + 1];
-          this.heap[(start * 2) + 1] = lower;
-          this.heap[start] = higher;
-          rec((start * 2) + 1);
-        }
-        rec(start + 1);
-      };
-      rec(1);
-    }
-  }
 }
 
-const heap = new Heap();
-heap.insert(5);
+module.exports = Heap;
