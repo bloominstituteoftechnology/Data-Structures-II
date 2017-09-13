4e4f02772c5c38d3be3a0d3dbb9fa30f48c06725
Nick Costigan <nick.costigan@gmail.com>
2017-09-13 19:55:18 -0400

    finished tree implementation

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,25 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    let found = false;
+    const searchTree = (children) => {
+      children.forEach((child) => {
+        if (child.value === value) {
+          found = true;
+        }
+        if (child.children) {
+          searchTree(child.children);
+        }
+      });
+    };
+    searchTree(this.children);
+    return found;
   }
 }
 
