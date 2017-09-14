a8575978ff3035570ed64736dd95d110491edf24
Joe Roland <joroland@gmail.com>
2017-09-13 20:53:35 -0400

    Finishg Tree

--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,26 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+
+  addChild(value) {
+    const childNode = new Tree(value);
+    this.children.push(childNode);
+  }
+
+  contains(value, node) {
+    node = node || this;
+
+    if (node.value === value) return true;
+
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i++) {
+        if (this.contains(value, node.children[i])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
 
 module.exports = Tree;
