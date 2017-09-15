9a9f0e10f1c3d6f74cfb14e45682764ee6ffdc00
Darryl Green <darryl.alexander.green@gmail.com>
2017-09-15 13:20:26 -0500

    All finshed save EC

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,6 +2,23 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+class Queue {
+  constructor() {
+    this.storage = [];
+  }
+
+  enqueue(x) {
+    this.storage.push(x);
+  }
+
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+}
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,26 +29,63 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let found = false;
+    if (this.value === target) {
+      return true;
+    } else if (this.value === null) {
+      return false;
+    } else if (target < this.value && this.left !== null) {
+      found = this.left.contains(target);
+    } else if (this.right !== null) {
+      found = this.right.contains(target);
+    } return found;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if (this.left !== null) {
+      this.left.depthFirstForEach(cb);
+    } if (this.right !== null) {
+      this.right.depthFirstForEach(cb);
+    }
   }
+  
+  
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
+  /* eslint-disable global-require */
   breadthFirstForEach(cb) {
-
+    const q = new Queue();
+    q.enqueue(this);
+    while (!q.isEmpty()) {
+      const next = q.dequeue();
+      cb(next.value);
+      if (next.left !== null) {
+        q.enqueue(next.left);
+      } if (next.right !== null) {
+        q.enqueue(next.right);
+      }
+    }
   }
 }
 
