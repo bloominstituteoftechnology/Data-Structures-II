f1f8ebf66d1858a229965993fe55a938ac33d0c6
Collin Ferguson <theredcouchroom@gmail.com>
2017-09-18 12:34:24 -0500

    all done but no heap

--- src/graph.js
+++ src/graph.js
@@ -40,17 +40,34 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
+    if (this.vertices.length === 1) {
+      const newNode = new GraphNode({ value, edges });
+      this.vertices.push(newNode);
 
+      // this.vertices[0]._edges = newNode;
+      this.addEdge(this.vertices[0], this.vertices[1]);
+      return newNode;
+    } 
+    const newNode = new GraphNode({ value, edges });
+    this.vertices.push(newNode);
+    return newNode;
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i]._value === value) return true;
+    }
+    return false;
   }
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   removeVertex(value) {
-
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i]._value === value) {
+        this.vertices.splice(i, 1);
+      }
+    }
   }
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
@@ -58,17 +75,44 @@ class Graph {
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    if (fromVertex._edges) {
+      for (let i = 0; i < fromVertex._edges.length; i++) {
+        if (fromVertex._edges[i] === toVertex) return true;
+      }
+    }
+    return false;
   }
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
+    if (!fromVertex._edges) {
+      fromVertex._edges = [];
+    }
+    if (!toVertex._edges) {
+      toVertex._edges = [];
+    }
+    if (!this.checkIfEdgeExists(fromVertex, toVertex)) {
+      fromVertex.pushToEdges(toVertex);
+      toVertex.pushToEdges(fromVertex);
+    }
   }
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   removeEdge(fromVertex, toVertex) {
-
+    if (this.checkIfEdgeExists(fromVertex, toVertex)) {
+      for (let i = 0; i < fromVertex._edges.length; i++) {
+        if (fromVertex._edges[i] === toVertex) {
+          fromVertex._edges.splice(i, 1);
+        }
+      }
+      for (let i = 0; i < toVertex._edges.length; i++) {
+        if (toVertex._edges[i] === fromVertex) {
+          toVertex._edges.splice(i, 1);
+        }
+      }
+    }
+    fromVertex._value = undefined;
+    toVertex._value = undefined;
   }
 }
 
--- src/tree.js
+++ src/tree.js
@@ -5,15 +5,29 @@ class Tree {
     this.value = value;
     this.children = [];
   }
-  // Adds a new Tree node with the input value to the current Tree node 
+  // Adds a new Tree node with the input value to the current Tree node
   addChild(value) {
-
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    let doesContain = false;
+    if (this.value === value) {
+      doesContain = true;
+      return doesContain;
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      // if (this.children[i].value === value) {
+      //   doesContain = true;
+      //   return doesContain;
+      // }
+      const a = this.children[i];
+      doesContain = this.contains.call(a, value);
+      if (doesContain) return doesContain;
+    }
+    return doesContain;
   }
 }
 
