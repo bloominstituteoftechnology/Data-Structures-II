e110c6333ae045f25b63dee7eff7fc2491626a24
ceylonsullen <hussainaljahmi@gmail.com>
2017-09-18 14:12:55 -0400

    no EC

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,6 +2,26 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+class Queue {
+  constructor() {
+    this.storage = [];
+  }
+
+  enqueue(x) {
+    this.storage.push(x);
+  }
+
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+}
+
+module.exports = new Queue();
+
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,17 +32,39 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    // if (typeof value === 'integer'){
+    if (value >= this.value) {
+      if (this.right) this.insert.call(this.right, value);
+      else this.right = new BinarySearchTree(value);
+    }
+    if (value < this.value) {
+      if (this.left) this.insert.call(this.left, value);
+      else this.left = new BinarySearchTree(value);
+    }
+    // }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let flag = false;
+    if (this.value === target) {
+      flag = true;
+      return flag;
+    }
+    if (target > this.value) {
+      if (this.right) flag = this.contains.call(this.right, target);
+    }
+    if (target < this.value) {
+      if (this.left) flag = this.contains.call(this.left, target);
+    }
+    return flag;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);    
+    if (this.left) this.depthFirstForEach.call(this.left, cb);
+    if (this.right) this.depthFirstForEach.call(this.right, cb);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -32,8 +74,26 @@ class BinarySearchTree {
   // again. Whatever floats your boat.
   /* eslint-disable global-require */
   breadthFirstForEach(cb) {
-
+    const q = new Queue();
+    let distance = 0;
+    const que = (x) => {
+      distance++;
+      if (q.isEmpty() && (x.value)) {
+        q.enqueue([x.value, distance]);
+        distance++;
+      }      
+      if (x.left && x.left.value) q.enqueue([x.left.value, distance]);
+      if (x.right && x.right.value) q.enqueue([x.right.value, distance]);
+      if (x.left) que(x.left);
+      if (x.right) que(x.right);
+    };
+    que(this);
+    while (q.isEmpty() === false) {
+      const store = q.dequeue();
+      cb(store[0]);
+    }
   }
 }
 
 module.exports = BinarySearchTree;
+
--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,23 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    let flag = false;
+    if (this.value === value) {
+      flag = true;
+      return flag;
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      const inside = this.children[i];
+      flag = this.contains.call(inside, value);
+      if (flag) return flag;
+    }
+    return flag;
   }
 }
 
