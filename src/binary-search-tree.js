d9b87346e9b39f79c6f7c3a07e36212601920893
Keagan Goetsch <misterrorypond@gmail.com>
2017-09-18 09:48:05 -0500

    binary tree commit

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,50 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
+    const newNode = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = newNode;
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (!this.right) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value);
+      }
+    }
 
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) {
+      return true;
+    }
+    if (this.left) {
+      if (this.left.contains(target)) {
+        return true;
+      }
+    }
+    if (this.right) {
+      if (this.right.contains(target)) {
+        return true;
+      }
+    }
+    return false;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if (this.right) {
+      this.right.depthFirstForEach(cb);
+    }
+    if (this.left) {
+      this.left.depthFirstForEach(cb);
+    }
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,8 +64,18 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
-  }
+    const current = [node];
+    
+        while (current.length > 0) {
+          const next = [];
+          for (const node of current) {
+            cb(node);
+            if (node.left) next.push(node.left);
+            if (node.right) next.push(node.right);
+          }
+          current = next;
+        }
+      }
 }
 
 module.exports = BinarySearchTree;
