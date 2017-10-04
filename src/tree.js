7697399efdc13d9ea990dbb82eac23e09455f9cc
Mark Oliver <mark.c.oliver@gmail.com>
2017-10-03 23:03:45 -0400

    tree.test.js passes

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,21 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(new Tree(value))
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
-  // have been checked
+  // have been checkedn
   contains(value) {
-
+    if (this.value === value) {
+      return true
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(value)) {
+        return true
+      }
+    }
+    return false    
   }
 }
 
