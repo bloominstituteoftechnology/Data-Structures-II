4e8560ee44b5807c66088e0da6d125dc4203399c
David Long <davdaarn@live.com>
2017-10-12 13:49:42 -0500

    changed before lunch

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,34 +2,48 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable class-methods-use-this */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-  // Wraps the input value in a new BinarySearchTree and
-  // assigns it to either the left or right subtree,
-  // depending on its value
+
   insert(value) {
-
+    if (value >= this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);      
+      } else {
+        this.left.insert(value);
+      }
+    }
   }
-  // Checks the binary search tree for the input target
-  // Can be written recursively or iteratively
+
   contains(target) {
-
+    if (target === this.value) return true;
+    if (target >= this.value && this.right !== null) {
+      if (target === this.right.value) return true;
+      this.right.contains(target);
+    }
+    if (target < this.value && this.left !== null) {
+      if (target === this.left.value) return true;
+      this.left.contains(target);
+    }
+    return false;
   }
-  // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
+
   depthFirstForEach(cb) {
 
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
-  // at the root node, going down to the root node's children, and iterating
-  // through all those nodes first before moving on to the next layer of nodes
-  // Applies the given callback to each tree node in the process
-  // You'll need the queue-helper file for this. Or could you roll your own queue
-  // again. Whatever floats your boat.
+
   breadthFirstForEach(cb) {
 
   }
