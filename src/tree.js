f2288effad3e6d4f16c8e3113d1467ddd8c6b5d1
Kay Ueda <Lizzy@Kays-MacBook-Pro.local>
2017-10-12 11:04:42 -0700

    tree draft complete

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,16 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(value);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) return true;
+    for (let k = 0; k < this.children.length; k++) {
+      if (this.children[k] !== []) return contains(this.children[k])
+    }
   }
 }
 
