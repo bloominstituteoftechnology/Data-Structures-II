c32b0e9691f67a6996a3e4de7c6ce1a0f55deca1
Alex Cassell <alex@simulism.net>
2017-10-12 16:28:12 -0400

    Tree is finished. Wokring on Binary Tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,7 +12,21 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const node = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = node;
+      } else {
+        return this.left.insert(value);
+      }
+    }
+    if (value >= this.value) {
+      if (this.right === null) {
+        this.right = node;
+      } else {
+        return this.right.insert(value);
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
