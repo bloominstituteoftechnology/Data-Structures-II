9788e4be3b64924f428316a49ff07ac850fbbbe0
Teresa Strout <qapla47@gmail.com>
2017-10-11 21:33:32 -0400

    trees all run!

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,30 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+// traverse the node and inspect children
+// determine if any of the children match our input value
+    if (this.value === value) {
+      return true;
+    } 
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(value)) {
+        return true;
+      } 
+    } return false;
+// else if
+// this.children !== null 
+// then for ( let i = 0; i < this.children.length; i++) {
+// iterate through the children and compare to value
+// this.children[i].contains(value)
+// this.children[0] returns all of the children at index 0
+// 
+// }
   }
 }
 
