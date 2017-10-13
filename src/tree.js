d13b40dcf2e95000b574e285d1b6bcd9d10f052f
Perry Ahern <pahern@gmail.com>
2017-10-12 20:35:41 -0400

    tree.js working - add and contains

--- src/tree.js
+++ src/tree.js
@@ -7,19 +7,23 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-    console.log('Test changes in and then some git dang lady. where are we? ');
-    // changes to test git moving...
-
-    // more changes to test conflict resolution
-    // and then some just in case
-    // and more changes from me, lorin
-
+    const node = new Tree(value);
+    this.children.push(node);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) return true;
+    let containsValue = false;
+    const searchTree = (treeNode) => {
+      for (let i = 0; i < treeNode.children.length; i++) {
+        if (treeNode.children[i].value === value) containsValue = true;
+        if (treeNode.children[i].children.length > 0) searchTree(treeNode.children[i]);
+      }  
+    };
+    searchTree(this);
+    return containsValue;
   }
 }
 
