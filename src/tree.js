eafb36a1c3f3ff1221f9e3d64213b612bfe0438e
codealtgeek <codealtgeek@gmail.com>
2017-10-12 21:05:20 -0400

    BST Finished

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,28 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const tree = new Tree(value);
+    this.children.push(tree);
   }
+
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
+    let containsValue = false;
 
+    const search = (children) => {
+      children.forEach((child) => {
+        if (value === child.value) {
+          return containsValue = true;
+        }
+        if (child.children.length) {
+          search(child.children);
+        }
+      });
+    };
+    search(this.children);
+    return containsValue;
   }
 }
 
