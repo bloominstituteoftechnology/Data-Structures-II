8810fa231d903ed69bef5ebea1522ec6dbdbb735
dan volosnikov <danvolosnikov@gmail.com>
2017-10-12 20:11:07 -0400

    added insert & contains

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,12 +12,38 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-    return this;
+    const newTree = new BinarySearchTree(value);
+    const helper = (curTree) => {
+      if (curTree.value > newTree.value && curTree.left !== null) {
+        helper(curTree.left);
+      } else if (curTree.value <= newTree.value && curTree.right !== null) {
+        helper(curTree.right);
+      } else if (curTree.value > newTree.value && curTree.left === null) {
+        curTree.left = newTree;
+      } else if (curTree.value <= newTree.value && curTree.right === null) {
+        curTree.right = newTree;
+      }
+    };
+    helper(this);    
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-    return this;
+    let result = false;
+    const helper = (curTree) => {
+      if (curTree.value === target) {
+        result = true;
+        return;
+      }
+      if (curTree.left !== null) {
+        helper(curTree.left);
+      }
+      if (curTree.right !== null) {
+        helper(curTree.right);
+      }
+    };
+    helper(this);
+    return result;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
