775492ff08203369c9f013a9963ee7597a2a85a4
Bhavik <ravanibhavik@gmail.com>
2017-10-12 20:48:53 -0400

    Updated Binary Search Tree and Tree.

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,34 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) return true;
+    else if (target < this.value && this.left !== null) return this.left.contains(target);
+    else if (target > this.value && this.right !== null) return this.right.contains(target);
+    else return false; 
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    if (this !== null) cb(this.value);
+    if (this.left !== null) this.left.depthFirstForEach(cb);
+    if (this.right !== null) this.right.depthFirstForEach(cb);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,8 +48,33 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    
   }
 }
 
-module.exports = BinarySearchTree;
+// module.exports = BinarySearchTree;
+
+
+binarySearchTree = new BinarySearchTree(10);
+// bst.insert(11);
+// bst.insert(20);
+// bst.insert(5);
+// bst.insert(6);
+// bst.insert(2);
+// bst.insert(8);
+// bst.insert(9);
+// bst.insert(20);
+// bst.insert(18);
+// bst.insert(25);
+// console.log(bst);
+// console.log(bst.contains(7));
+
+
+const array = [];
+const foo = value => ((array.push(value)));
+binarySearchTree.insert(2);
+binarySearchTree.insert(3);
+binarySearchTree.insert(7);
+binarySearchTree.insert(9);
+binarySearchTree.depthFirstForEach(foo);
+console.log(array);
--- src/tree.js
+++ src/tree.js
@@ -5,16 +5,63 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const node = new Tree(value);
+    this.children.push(node);
   }
+  
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
-  contains(value) {
-
+  contains(value) { // pass in this.children[0][1]
+    const currentValue = this.value;
+    const currentChildren = this.children;
+    
+    const containsValue = function (val, child) {
+      console.log(val);
+      console.log(child);
+      if (val === value) return true;
+      else if (child.length > 0) {
+        for (let i = 0; i < child.length; i++) {
+          return containsValue(child[i].value, child[i].children); 
+        }
+      }
+    }
+    
+    if (containsValue(currentValue, currentChildren)) return true;
+    else return false;
   }
 }
 
-module.exports = Tree;
+
+//   contains(value) {
+//     if (this.value === value) {
+//       return true;
+//     }
+//     for (let i = 0; i < this.children.length; i++) {
+//       if (this.children[i].contains(value)) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+// }
+
+// module.exports = Tree;
+
+
+tree = new Tree(10);
+// console.log(tree);
+tree.addChild(5);
+// console.log(tree);
+tree.children[0].addChild(6);
+console.log(tree.contains(6));
+
+// tree = {value, children: [
+//   {
+//     value, children: []
+//   }
+// ]
+// }
