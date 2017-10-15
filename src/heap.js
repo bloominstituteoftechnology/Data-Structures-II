00e9fd816de380c6b598916147f8c5a97581c27c
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-14 21:25:25 -0700

    passing heap

--- src/heap.js
+++ src/heap.js
@@ -10,6 +10,7 @@ class Heap {
       if (this.getSize() === 0) {
         this.storage[this.getSize()] = val;
       } else {
+        // console.log(val);
         this.storage.push(val);
         this.bubbleUp(this.getSize());
       }
@@ -19,20 +20,17 @@ class Heap {
     // Calls siftDown in order to reorganize the heap with a new max/min
     // In some specifications, this method is also called `poll`
     delete() {
-      const retVal = this.getStorage()[0];
-      console.log(retVal);
-      console.log(this.storage);
-      this.storage[0] = this.storage[this.getSize()];
-      this.storage.splice(this.getSize(), 1);
       this.size--
+      if (this.getSize() < 0) return;
+      //console.log(this.getStorage()[this.getSize() - 1])
+      this.swap(0, this.getSize());
+      // console.log(this.getStoragethis.getSize)
+      const retValue = this.storage.pop();
       this.siftDown(0);
-      if (this.size === 0) this.storage[0] = null;
-      //console.log(retVal);
-      return retVal;
+      return retValue;
     }
     // Returns the maximum value in the heap in constant time
     getMax() {
-      if (this.getSize() === 1) return null;
       return this.getStorage()[0];
     }
     // Returns the size of the heap
@@ -48,38 +46,29 @@ class Heap {
     // if its parent value is less than the value located at the input index
     // This method is only used by the heap itself in order to maintain the heap property
     bubbleUp(index) {
-      let parIndex;
-      if (index%2 === 0) {
-        parIndex = (index - 2)/2;
-      } else {
-        parIndex = (index - 1)/2;
-      }
-      if (this.getStorage()[parIndex] < this.getStorage()[index]) {
-        this.swap(index, parIndex);
-        this.bubbleUp(parIndex);            
-      }
+      const n = index%2 === 0 ? 2 : 1;
+      const parIndex = ((index - n)/2)
+      const parent = this.getStorage()[parIndex];
+      const checkValue = this.getStorage()[index];
+      if (parent > checkValue) return;
+      if (parent < checkValue) this.swap(index, parIndex);
+      if (parent !== undefined) this.bubbleUp(parIndex);
     }
     // First grabs the indices of this element's children and determines which of the children are larger
     // If the larger of the child elements is larger than the parent, the child element is swapped with the parent
     // This method is only used by the heap itself in order to maintain the heap property
     siftDown(index) {
-      const parent = this.storage[index];
-      const leftChild = this.storage[2 * index + 1];
-      const rightChild = this.storage[2 * index + 2];
-
-      if (leftChild > parent && leftChild >= rightChild) {
-        //parent and left child swap
-        const temp = parent;
-        this.storage[index] = leftChild;
-        this.storage[2*index+1] = parent;
-        this.siftDown(2*index+1);        
-      } else if (rightChild > parent && rightChild > leftChild) {
-        const temp = parent;
-        this.storage[index] = rightChild;
-        this.storage[2*index+2] = parent;
-        this.siftDown(2*index+2);        
-      }
+      if (this.getSize() === 0 || index == this.getSize()) return;
+      const checkValue = this.getStorage()[index];
+      const left = this.getStorage()[(index*2)+1] || null;
+      const right = this.getStorage()[(index*2)+2] || null;
+      if (left === null && right === null) return;
+      const bigger = left > right ? left : right;
+      if (checkValue === bigger || checkValue > bigger) return;
+      if (bigger > checkValue) this.swap(this.getStorage().indexOf(bigger), index);
+      this.siftDown(this.getStorage().indexOf(checkValue))
     }
+
     swap(index, withIndex) {
       const temp = this.getStorage()[index];
       this.storage[index] = this.storage[withIndex];
