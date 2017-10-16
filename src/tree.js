c121d2c7f29c08ff763bd45ed1062131088c59fd
JaredRCooper <jaredrcoopet@gmail.com>
2017-10-16 19:09:06 -0400

    graph, tree, and binary tree completed, wasn't able to do 4th...

--- src/tree.js
+++ src/tree.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
 class Tree {
@@ -7,13 +8,21 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-    this.children.push(value);
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) {
+      return true;
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(value)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
