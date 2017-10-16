44c4b5a19d7f9fdad2d0d114e2dc40744b72625f
Ronald Willis II <32463687+oggiedoggy12@users.noreply.github.com>
2017-10-16 11:58:29 -0500

    Update tree.js

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,25 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const child = new Tree(value);
+    child.value = value;
+    this.children.push(child);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
+    const stack = [this];
+    let n;
+    while (stack.length > 0) {
+      n = stack.pop();
+      if (n.value === value) return true;
 
+      for (let i = n.children.length -1; i >= 0; i--) {
+        stack.push(n.children.length[i]);
+      }
+    }
+    return false;
   }
 }
 
