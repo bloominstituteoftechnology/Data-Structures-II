e5a932f1f8e32efd1e4fd607fa2ca935eacb602b
Sondro <memoiran@hotmail.com>
2017-10-18 11:32:24 -0700

    Initial commit

--- src/graph.js
+++ src/graph.js
@@ -3,75 +3,132 @@
 /* eslint-disable no-trailing-spaces */
 // Do not modify this GraphNode class
 // Use any of its methods as you see fit to implement your graph
+// ----------------------------------------------------------------
 class GraphNode {
   constructor({ value, edges }) {
     this._value = value;
     this._edges = edges;
   }
-
+  // --------------------------------------------------------------
   get value() {
     return this._value;
   }
-
+  // --------------------------------------------------------------
   get edges() {
     return this._edges;
   }
-
+  // --------------------------------------------------------------
   get numberOfEdges() {
     return this._edges.length;
   }
-
+  // --------------------------------------------------------------
   set edges(x) {
     this._edges = x;
   }
-
+  // --------------------------------------------------------------
   pushToEdges(y) {
     this._edges.push(y);
   }
+  // --------------------------------------------------------------
 }
-
+// ----------------------------------------------------------------
 class Graph {
   constructor() {
     this.vertices = [];
   }
+  // --------------------------------------------------------------
   // Wraps the input value in a new GraphNode and adds it to the array of vertices
   // If there are only two nodes in the graph, they need to be automatically 
   // connected via an edge
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
+  // ---------------------------------------------------------------
   addVertex(value, edges = []) {
-
+    const node = new GraphNode({ value, edges: [] });
+    this.vertices.push(node);
+    // -------------------------------------------------------------
+    if (this.vertices.length === 2) {
+      this.addEdge(this.vertices[0], this.vertices[1]);
+    }
+    // -------------------------------------------------------------
+    if (edges !== []) {
+      let i = 0;
+      for (; i < edges.length; i++) {
+        this.addEdge(node, edges[i]);
+      }
+    }
+    // -------------------------------------------------------------
+    return node;
   }
+  // ---------------------------------------------------------------
   // Checks all the vertices of the graph for the target value
   // Returns true or false
+  // ---------------------------------------------------------------
   contains(value) {
-
+    let i = 0;
+  // ---------------------------------------------------------------
+    for (; i < this.vertices.length; i++) { // this was really easy loop through the list and find value
+      return this.vertices[i].value === value;
+    }
+    return false;
   }
+  // ---------------------------------------------------------------
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-
+    let reVertex;
+    let i = 0;
+    // -------------------------------------------------------------
+    for (; i < this.vertices.length; i++) {
+      if (this.vertices[i].value === value) reVertex = this.vertices.splice(i, 1);
+    }
+    // -------------------------------------------------------------
+    if (!reVertex) {
+      i = 0;
+      // ----------------------------------------------------------- 
+      for (; i < reVertex.numberOfEdges; i++) {
+        this.removeEdge(reVertex, reVertex.edges[i]);
+      }
+      // -----------------------------------------------------------
+    }
+    // -------------------------------------------------------------
   }
+  // ---------------------------------------------------------------
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
   // If only one vertex references the other but not vice versa, should not return true
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
+  // ---------------------------------------------------------------
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    return fromVertex.edges.includes(toVertex) && toVertex.edges.includes(fromVertex); 
   }
+  // ---------------------------------------------------------------
   // Adds an edge between the two given vertices if no edge already exists between them
-  // Again, an edge means both vertices reference the other 
+  // Again, an edge means both vertices reference the other
+  // --------------------------------------------------------------- 
   addEdge(fromVertex, toVertex) {
-
+    fromVertex.pushToEdges(toVertex);
+    toVertex.pushToEdges(fromVertex);
   }
+  // ---------------------------------------------------------------
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   // If a vertex would be left without any edges as a result of calling this function, those
   // vertices should be removed as well
+  // ---------------------------------------------------------------
   removeEdge(fromVertex, toVertex) {
-
+    // -------------------------------------------------------------
+    if (this.checkIfEdgeExists(fromVertex, toVertex)) {
+      // -----------------------------------------------------------
+      fromVertex.edges = fromVertex.edges.filter(v => v.value !== toVertex.value);
+      toVertex.edges = fromVertex.edges.filter(v => v.value !== fromVertex.value);
+      // -----------------------------------------------------------
+      if (fromVertex.numberOfEdges === 0) { this.removeVertex(fromVertex.value); }
+      if (toVertex.numberOfEdges === 0) { this.removeVertex(toVertex.value); }
+    }
+    // -------------------------------------------------------------
   }
 }
 
--- src/heap.js
+++ src/heap.js
@@ -1,44 +1,88 @@
 /* eslint-disable */
+// ----------------------------------------------------------------
 class Heap {
+    // ------------------------------------------------------------
     constructor() {
-        this.storage = [null];
-        this.size = 0;
+      this.storage = [null];
+      this.size = 0;
     }
+    // ------------------------------------------------------------
     // Inserts the given value in the heap
     // Calls bubbleUp in order to put the newly-inserted element in the right place in the heap
+    // ------------------------------------------------------------
     insert(val) {
-
+      this.size++;
+      if (this.getSize() === 0) {
+        this.storage[this.getSize()] = val;
+      } else {
+        this.storage.push(val);
+        this.bubbleUp(this.getSize());
+      }
     }
+    // ------------------------------------------------------------
     // Deletes the element located at the front of the heap (the max if a max heap, or a min if a min heap)
     // Calls siftDown in order to reorganize the heap with a new max/min
     // In some specifications, this method is also called `poll`
+    // ------------------------------------------------------------
     delete() {
-
+      this.size--;
+  
+      if (this.getSize() > 0) {
+  
+      }
+  
     }
+    // ------------------------------------------------------------
     // Returns the maximum value in the heap in constant time
     getMax() {
-
+      return this.getStorage()[0];
     }
+    // ------------------------------------------------------------
     // Returns the size of the heap
     getSize() {
-
+      return this.size;
     }
+    // ------------------------------------------------------------
     // Returns the storage array
     getStorage() {
-
+      return this.storage;
     }
+    // ------------------------------------------------------------
     // Moves the element at the specified index "up" by swapping it with its parent 
     // if its parent value is less than the value located at the input index
     // This method is only used by the heap itself in order to maintain the heap property
     bubbleUp(index) {
-
+      const storageTmp = this.storage[Math.floor(index / 2)];
+      this.storage[Math.floor(index / 2)] = this.storage[index];
+      this.storage[index] = storageTmp;
     }
+    // -------------------------------------------------------------
     // First grabs the indices of this element's children and determines which of the children are larger
     // If the larger of the child elements is larger than the parent, the child element is swapped with the parent
     // This method is only used by the heap itself in order to maintain the heap property
+    // -------------------------------------------------------------
     siftDown(index) {
-      
+      if (this.getSize() != 0 || index !== this.getSize()) {
+        // ---------------------------------------------------------
+        const check = this.getStorage()[index];
+        const left = this.getStorage()[(index * 2) + 1] || null;
+        const right = this.getStorage()[(index * 2) + 2] || null;
+        // ---------------------------------------------------------
+        if (left !== null && right !== null) {
+          const bigger = left > right ? left : right;
+          if (checkValue === bigger || checkValue > bigger) {
+            if (bigger > checkValue) {
+              this.swap(this.getStorage().indexOf(bigger), index);
+            }
+            this.siftDown(this.getStorage().indexOf(checkValue));
+          }
+        }
+      }
     }
-}
-
-module.exports = Heap;
+    // -------------------------------------------------------------
+  }
+  // ----------------------------------------------------------------
+  /*
+   */
+  // ----------------------------------------------------------------
+  module.exports = Heap;
