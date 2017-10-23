4a6634837e90e1deec4f87eb387a1a3c745ef518
alexfigliolia <alexfigliolia@gmail.com>
2017-10-23 13:36:34 -0400

    udpate

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,36 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if(value < this.value) {
+      if(this.left) {
+        this.insert(value);
+      } else {
+        this.left = new BinarySearchTree(value);
+      }
+    } else {
+      if(this.right) {
+        this.insert(value);
+      } else {
+        this.right = new BinarySearchTree(value);
+      }
+    }
+    //Recursion will create the new BST when insert is called
+    //from within itself
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if(this.value === target) return true;
+    if(this.left && this.left.contains(target)) return true;
+    if(this.right && this.right.contains(target)) return true;
+    return false;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if(this.left) this.depthFirstForEach(cb);
+    if(this.right) this.right.depthFirstForEach(db);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,7 +50,13 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = [];
+    queue.push(this);
+    const queueLength = gueue.length;
+    for(let i = 0; i<queueLength; i++) {
+      if(queue[i].left) queue.push(queue[i].left);
+      if(queue[i].right) queue.push(queue[i].right);
+    }
   }
 }
 
