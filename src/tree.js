fcad5e8b1df3bc24193c5108da3a323926c00750
dreamingrainbow <michaeladennis@yahoo.com>
2017-11-15 16:01:58 -0700

    finished tree

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,22 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const newTree = new Tree(value);
+    this.children.push(newTree);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.children.length === 0 && this.value !== value) return false;
+    if (this.value === value) return true;
+    let s = false;
+    this.children.forEach((i) => {
+      if (i.contains(value)) {
+        s = true;
+      }
+    });
+    return s;
   }
 }
 
