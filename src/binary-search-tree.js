7aadeacc98dc3894edbdeac17ea1e8adbde3d413
FJF616 <franzjfernando@hotmail.com>
2017-11-15 15:40:55 -0800

    completed tree.js and most of binary-search-trees.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,46 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const newBT = new BinarySearchTree(value);
+    if (this.value === undefined) {
+      this.value = newBT;
+    }
+    if (this.value > value && this.left === undefined) {
+      this.left = newBT;
+    } else if (this.value < value && this.right === undefined) {
+      this.right = newBT;
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let foundVal = false;
+    const recurse = (value) => {
+      value = this.value;
+      if (target === value) {
+        foundVal = true;
+      } else if (this.left !== undefined && value > target) {
+        recurse(this.left);
+      } else if (this.right !== undefined && value < target) {
+        recurse(this.right);
+      }
+    };
+    recurse(this);
+    return foundVal;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    const recurse = (value) => {
+      cb.call(value, this.value);
+      if (this.left !== undefined) {
+        recurse(this.left);
+      }
+      if (this.right !== undefined) {
+        recurse(this.right);
+      }
+    };
+    recurse(this);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,21 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const newNode = new Tree(value);
+    this.children.push(newNode);
+    return newNode;
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) {
+      return true;
+    } 
+    this.children.forEach((child) => {
+      if (child && child.contains(value)) return true;
+    });
+    return false;
   }
 }
 
