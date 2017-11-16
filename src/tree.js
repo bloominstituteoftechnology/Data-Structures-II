8a41ab89d8929d3947ec6f2efd9fcacc0e3db929
Ting Wang <felix.wang.1026@gmail.com>
2017-11-16 15:37:11 -0500

    Refactored the codes

--- src/tree.js
+++ src/tree.js
@@ -14,7 +14,6 @@ class Tree {
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-    if (this.value === value) return true;
     if (this.children.length === 0) return this.value === value;
     let flag = false;
     this.children.forEach((child) => {
