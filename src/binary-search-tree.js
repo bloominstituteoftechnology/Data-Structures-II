20ed24ed065b10a004caf567625909f9c317f0c8
Aaron Wilder <element1028@gmail.com>
2017-11-15 17:52:59 -0800

    2 tests passing on b-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,30 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-    
+    const node = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = node;
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = node;
+      } else {
+        this.right.insert(value);
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-    
+    this.left = 1;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-    
+    this.left = 1;
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,7 +44,7 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-    
+    this.left = 1;
   }
 }
 
