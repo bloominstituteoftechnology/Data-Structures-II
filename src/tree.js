64339e9bcf21e069feb7b2b386db9455d4a2f8a2
Ting Wang <felix.wang.1026@gmail.com>
2017-11-15 20:17:33 -0500

    Completed the tree requirements

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,20 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const child = new Tree(value);
+    this.children.push(child);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) return true;
+    if (this.children.length === 0) return this.value === value;
+    let flag = false;
+    this.children.forEach((child) => {
+      if (child.contains(value)) flag = true; 
+    });
+    return flag;
   }
 }
 
