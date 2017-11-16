4355ff8bb20a84c72b8758c4655ed128a5d3ea41
ubuntu <irishseanvaldivia@gmail.com>
2017-11-16 13:47:41 -0600

    Let's see if this works...

--- src/tree.js
+++ src/tree.js
@@ -13,7 +13,14 @@ class Tree {
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    if (this.value === value) return true;
+    if (this.children.length > 0) {
+      for (let i = 0; i < this.children.length; i++) {
+        if (this.children[i].contains(value)) return true;
+      }
+    } else {
+      return false;
+    }
   }
 }
 
