f5fed88f32cd5ed757b38f04b759e2424187024e
Sarika <sarikaresume@gmail.com>
2017-11-17 12:10:20 -0800

    wild wild thoughts

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -14,7 +14,6 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-<<<<<<< HEAD
     if (value < this.value) {
       if (this.left === null) {
         this.left = new BinarySearchTree(value);
@@ -26,28 +25,12 @@ class BinarySearchTree {
     this.right = new BinarySearchTree(value);
     } else {
       this.right.insert(value);
-=======
-    const node = new BinarySearchTree(value);
-    if (value < this.value) {
-      if (this.left === null) {
-        this.left = node;
-      } else {
-        this.left.insert(value);
-      }
-    } else if (value > this.value) {
-      if (this.right === null) {
-        this.right = node;
-      } else {
-        this.right.insert(value);
-      }
->>>>>>> 20ed24ed065b10a004caf567625909f9c317f0c8
     }
   }
 }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-<<<<<<< HEAD
     if (this.value === target) return true;
     if (target < this.value) {
        if (this.left == null) return false; 
@@ -63,34 +46,22 @@ class BinarySearchTree {
     cb(this.value);
     if (this.left !== null) this.left.depthFirstForEach(cb);
     if (this.right !== null) this.right.depthFirstForEach(cb);
-=======
-    this.left = 1;
-  }
-  // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
-  depthFirstForEach(cb) {
-    this.left = 1;
->>>>>>> 20ed24ed065b10a004caf567625909f9c317f0c8
   }
 }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
-  // at the root node, going down to the root node's children, and iterating
+  // at the root node, going down to the node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-<<<<<<< HEAD
     const queue = [];
     queue.push(this);
-    for (let i = 0; i <queue.length; i++) {
+    forwhile (let i = 0; i <queue.length; i++) {
       cb(queue[i].value); 
       if (queue[i].left) queue.push(queue[i]).left);
       if (queue[i].right) queue.push(queue[i].right);
     }
-=======
-    this.left = 1;
->>>>>>> 20ed24ed065b10a004caf567625909f9c317f0c8
   }
 }
 
