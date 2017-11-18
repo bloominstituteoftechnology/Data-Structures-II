647afd3ca148656f4c6910bc2f3cebbf5a8ada4c
Jamie Domingo <jaybmw@aol.com>
2017-11-17 23:37:24 -0800

    Data-Structures-II Complete

--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable class-methods-use-this */
+
 // Do not modify this GraphNode class
 // Use any of its methods as you see fit to implement your graph
 class GraphNode {
@@ -37,21 +39,65 @@ class Graph {
   // Wraps the input value in a new GraphNode and adds it to the array of vertices
   // If there are only two nodes in the graph, they need to be automatically 
   // connected via an edge
-  // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
+  // Optionally accepts an array of other GraphNodes` for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
+    // const newVert = new GraphNode({ value, edges });
+    // for (let i = 0; i < edges.length; i++) {
+    //   this.addEdge(newVert, edges[i]);
+    const newNode = new GraphNode({ value, edges });
 
+    newNode.edges.forEach((edge) => {
+      this.addEdge(newNode, edge);
+    });
+
+    if (this.vertices.length === 1) {
+      this.addEdge(newNode, this.vertices[0]);
+    }
+
+    this.vertices.push(newNode);
+    return newNode;
+
+    // If the length of vertices equals 1, add both to the other's edges
+    // Push the new GraphNode to the Graph vertices
+    // Return the new GraphNode(Vertex)
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    // this.vertices.forEach(vertex => {
+    //   if (vertex.value === value) return true;
+    // });
+    // return false;
+    // }
+    return this.vertices.some(vertex => vertex.value === value);
   }
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
+    // for (let i = 0; i < this.value.length; i++) {
+    //   if (value === this.value[i]) {
+    //     this.value[i] = null;
+    //   }
+    // }
+    // this.checkIfEdgeExists(fromVertex, toVertex);
+    // if (this.contains(value)) return;
+    // const forRemoval = this.vertices.filter(vertex => vertex.value === value)[0];
 
+    const filteredVertices = [];
+    let vertexforDeletion;
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i].value === value) {
+        vertexforDeletion = this.vertices[i];
+      } else {
+        filteredVertices.push(this.vertices[i]);
+      }
+    }
+    this.vertices = filteredVertices;
+    vertexforDeletion.edges.forEach((vertex) => {
+      this.removeEdge(vertexforDeletion, vertex);
+    });
   }
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
@@ -59,21 +105,45 @@ class Graph {
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    return (fromVertex.edges.includes(toVertex) && toVertex.edges.includes(fromVertex));
   }
   // Adds an edge between the two given vertices if no edge already exists between them
-  // Again, an edge means both vertices reference the other 
+  // // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
+    // If fromVertex's edges does not include toVertex, push toVertex to fromVertex's edges
+    // If toVertex's edges does not include fromVertex, push fromVertex to toVertex's edges
+    // let fromVertContains = false;
+    // for (let i = 0; i < toVertex.edges.length; i++) {
+    //   if (fromVertex === toVertex.edges[i]) {
+    //     fromVertContains = true;
+    //   } 
+    // }
+    // if (!fromVertContains) {
+    //   toVertex.pushToEdges(toVertex);
+    // }
+    // let toVertContains = false;
+    // for (let j = 0; j < fromVertex.edges.length; j++) {
+    //   if (toVertex === fromVertex.edges[j]) {
+    //     toVertContains = true;
+    //   }
+    // }
+    // if (!toVertContains) {
+    //   fromVertex.pushToEdges(toVertex);
+    // }
+    if (!fromVertex.edges.includes(toVertex)) fromVertex.pushToEdges(toVertex);
+    if (!toVertex.edges.includes(fromVertex)) toVertex.pushToEdges(fromVertex);
   }
-  // Removes the edge between the two given vertices if an edge already exists between them
-  // After removing the edge, neither vertex should be referencing the other
-  // If a vertex would be left without any edges as a result of calling this function, those
-  // vertices should be removed as well
+  // // Removes the edge between the two given vertices if an edge already exists between them
+  // // After removing the edge, neither vertex should be referencing the other
+  // // If a vertex would be left without any edges as a result of calling this function, those
+  // // vertices should be removed as well
   removeEdge(fromVertex, toVertex) {
-
+    if (!this.checkIfEdgeExists(fromVertex, toVertex)) return false;
+    fromVertex.edges = fromVertex.edges.filter(vertex => vertex.value !== toVertex.value); // return boolean, any vertex that passed test then add array
+    toVertex.edges = toVertex.edges.filter(vertex => vertex.value !== fromVertex.value); // return boolean, any vertex that passed test then add array
+    if (fromVertex.numberOfEdges === 0) this.removeVertex(fromVertex.value);
+    if (toVertex.numberOfEdges === 0) this.removeVertex(toVertex.value);
   }
 }
 
 module.exports = Graph;
-
--- src/heap.js
+++ src/heap.js
@@ -1,44 +1,86 @@
 /* eslint-disable */
 class Heap {
-    constructor() {
-        this.storage = [null];
-        this.size = 0;
-    }
-    // Inserts the given value in the heap
-    // Calls bubbleUp in order to put the newly-inserted element in the right place in the heap
-    insert(val) {
+	constructor() {
+		this.storage = [null];
+		this.size = 0;
+	}
+	// Inserts the given value in the heap
+	// Calls bubbleUp in order to put the newly-inserted element in the right place in the heap
+	insert(val) {
+	  	this.storage.push(val)
+	  	this.bubbleUp(this.storage.length-1);
+	}
+	// Deletes the element located at the front of the heap (the max if a max heap, or a min if a min heap)
+	// Calls siftDown in order to reorganize the heap with a new max/min
+	// In some specifications, this method is also called `poll`
+	delete() {
+		this.storage.shift();
+	}
+	// Returns the maximum value in the heap in constant time
+	getMax() {
+		let max = 0;
+		for (let i = 0; i < this.storage.length; i++) {
+			if (max < this.storage[i]) {
+				max = this.storage[i];
+			}
+		}
+		return max;
+	}
+	// Returns the size of the heap
+	getSize() {
+		return this.storage.length;
+	}
+	// Returns the storage array
+	getStorage() {
+		return this.storage;
+	}
+	// Moves the element at the specified index "up" by swapping it with its parent 
+	// if its parent value is less than the value located at the input index
+	// This method is only used by the heap itself in order to maintain the heap property
+	bubbleUp(index) {
+		while (index>0) {
+			let parentN = Math.floor ((n+1) / 2) - 1,
+			parent = this.content[parentN];
+			if(score >= this.scoreFunction(parent)) {
+				break;
+			}
+			this.storage[parentN] = this.storage[index];
+			this.storage[n] = parent;
+			n = parentN;
 
-    }
-    // Deletes the element located at the front of the heap (the max if a max heap, or a min if a min heap)
-    // Calls siftDown in order to reorganize the heap with a new max/min
-    // In some specifications, this method is also called `poll`
-    delete() {
+		}
+	}
+	// First grabs the indices of this element's children and determines which of the children are larger
+	// If the larger of the child elements is larger than the parent, the child element is swapped with the parent
+	// This method is only used by the heap itself in order to maintain the heap property
+	siftDown(index) {
+		element = this.storage[index],
+		elemScore = this.scoreFunction(element);
 
-    }
-    // Returns the maximum value in the heap in constant time
-    getMax() {
-
-    }
-    // Returns the size of the heap
-    getSize() {
-
-    }
-    // Returns the storage array
-    getStorage() {
-
-    }
-    // Moves the element at the specified index "up" by swapping it with its parent 
-    // if its parent value is less than the value located at the input index
-    // This method is only used by the heap itself in order to maintain the heap property
-    bubbleUp(index) {
-
-    }
-    // First grabs the indices of this element's children and determines which of the children are larger
-    // If the larger of the child elements is larger than the parent, the child element is swapped with the parent
-    // This method is only used by the heap itself in order to maintain the heap property
-    siftDown(index) {
-      
-    }
+	 	while(true) {
+	  		let child2N = (n+1) * 2, child1N = child2N -1;
+	  		let swap = null;
+	  		if (child1N < length) {
+	  			let child1 = this.content(child1N);
+	  		}
+	  			child1Score = this.scoreFunction(child1);
+	  		if (childScore < elemScore) {
+	  			swap = child1N;
+	  		}
+	  	}
+	  	if (child2N < this.storage.length) {
+        	var child2 = this.storage[child2N],
+        	child2Score = this.scoreFunction(child2);
+        }
+        if (child2Score < (swap == null ? elemScore : child1Score))
+          	swap = child2N;
+      	// if (swap === null) {
+      	// 	break;
+      	// }
+      	this.storage[n] = this.content[swap];
+      	this.storage[swap] = element;
+      	n = swap;
+	}
 }
 
 module.exports = Heap;
