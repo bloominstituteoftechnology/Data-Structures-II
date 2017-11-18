5cec12b66a853635968a393ca9636632b9b15d59
Ting Wang <felix.wang.1026@gmail.com>
2017-11-17 21:01:26 -0500

    Refactored graph codes and add comments to graph

--- src/graph.js
+++ src/graph.js
@@ -41,19 +41,23 @@ class Graph {
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
     const vertex = new GraphNode({ value, edges });
+    // if there is only one vertex in the graph and the new vertex doesn't equal to existed vertex
     if (this.vertices.length === 1 && value !== this.vertices[0].value) {
+      // if there is no edge between vertices
       if (!this.vertices[0].edges.includes(vertex) && !vertex.edges.includes(this.vertices[0])) {
         this.vertices[0].pushToEdges(vertex);
         vertex.pushToEdges(this.vertices[0]);
-      } else if (!this.vertices[0].edges.includes(vertex)) {
+      } else if (!this.vertices[0].edges.includes(vertex)) { // if there is half edge
         this.vertices[0].pushToEdges(vertex);
-      } else if (!vertex.edges.includes(this.vertices[0])) {
+      } else if (!vertex.edges.includes(this.vertices[0])) { // if there is half edge
         vertex.pushToEdges(this.vertices[0]);
       }
       this.vertices.push(vertex);
-    } else if (this.vertices !== 0) {
+    } else if (this.vertices !== 0) { // if there are more than 1 vertices in graph
+      // if new vertex has edges
       if (edges.length !== 0) {
         edges.forEach((item) => {
+          // if vertices in graph are in new vertex's edges
           if (this.vertices.includes(item) && !item.edges.includes(vertex)) {
             this.vertices[this.vertices.indexOf(item)].pushToEdges(vertex);
           } 
@@ -77,7 +81,9 @@ class Graph {
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
     const vertex = this.vertices.find(item => item.value === value);
+    // if vertex exists
     if (vertex) {
+      // find the 
       vertex.edges.forEach((node) => {
         this.removeEdge(vertex, node);
       });
@@ -92,8 +98,11 @@ class Graph {
   checkIfEdgeExists(fromVertex, toVertex) {
     const fromEdges = fromVertex.edges;
     const toEdges = toVertex.edges;
+    // if both vertices are not in the graph
     if (!this.vertices.includes(fromVertex) || !this.vertices.includes(toVertex)) return false;
+    // if there is edge between vertices
     if (fromEdges.includes(toVertex) && toEdges.includes(fromVertex)) return true;
+    // otherwise there is no edge
     return false;
   }
   // Adds an edge between the two given vertices if no edge already exists between them
@@ -101,16 +110,21 @@ class Graph {
   addEdge(fromVertex, toVertex) {
     const fromEdges = fromVertex.edges;
     const toEdges = toVertex.edges;
+    // if vertices are not in the graph, do nothing
     if (!this.vertices.includes(fromVertex) || !this.vertices.includes(toVertex)) return undefined;
+    // if there is edge already, do nothing
     if (fromEdges.includes(toVertex) && toEdges.includes(fromVertex)) return undefined;
+    // if there is half edge
     if (fromEdges.includes(toVertex) && !toEdges.includes(fromVertex)) {
       toVertex.pushToEdges(fromVertex);
       this.vertices[this.vertices.indexOf(toVertex)] = toVertex;
     }
+    // if there is another half edge
     if (!fromEdges.includes(toVertex) && toEdges.includes(fromVertex)) {
       fromVertex.pushToEdges(toVertex);
       this.vertices[this.vertices.indexOf(fromVertex)] = fromVertex;
     }
+    // if there is no edge at all
     if (!fromEdges.includes(toVertex) && !toEdges.includes(fromVertex)) {
       fromVertex.pushToEdges(toVertex);
       toVertex.pushToEdges(fromVertex);
@@ -123,14 +137,17 @@ class Graph {
   // If a vertex would be left without any edges as a result of calling this function, those
   // vertices should be removed as well
   removeEdge(fromVertex, toVertex) {
+    // if there is edge 
     if (this.checkIfEdgeExists(fromVertex, toVertex)) {
       const fromEdges = fromVertex.edges;
       const toEdges = toVertex.edges;
       fromEdges.splice(fromEdges.indexOf(toVertex), 1);
+      // if fromVertex has no edge, remove it from graph
       if (fromEdges.length === 0) this.vertices.splice(this.vertices.indexOf(fromVertex), 1);
       fromVertex.edges = fromEdges;
       this.vertices[this.vertices.indexOf(fromVertex)] = fromVertex;
       toEdges.splice(toEdges.indexOf(fromVertex), 1);
+      // if toVertex has no edge, remove it from graph
       if (toEdges.length === 0) this.vertices.splice(this.vertices.indexOf(toVertex), 1);
       toVertex.edges = toEdges;
       this.vertices[this.vertices.indexOf(toVertex)] = toVertex;
