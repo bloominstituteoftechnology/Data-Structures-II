dfa538d43554aa1fe1e04d15d35599fc81d082f3
FJF616 <franzjfernando@hotmail.com>
2017-11-23 02:15:00 -0800

    more changes to heap

--- src/heap.js
+++ src/heap.js
@@ -18,47 +18,17 @@ class Heap {
     delete() {
         let delItem;
         this.storage[0] = delItem;
-        for (let i = 0; i < this.storage.length; i++) {
-            this.siftDown(i);
-        }
-       
-    }
-    // Returns index of left Child
-    getLeftChildIndex(parentIndex) {
-        return 2 * parentIndex + 1;
-    }
-    // Returns index of right child.
-    getRightChildIndex(parentIndex) {
-        return 2 * parentIndex +2;
-    }
-    // Returns parent index
-    getParentIndex(childIndex) {
-        return (childIndex - 1) / 2;
-    }
-    // Returns boolean value for left child
-    hasLeftChild(index) {
-        const myHeap = new Heap();
-        return (myHeap.getLeftChildIndex(index) < this.size);
-    }
-    // Returns boolean for child on right
-    hasRightChild(index) {
-        const myHeap = new Heap();
-        return (myHeap.getRightChildIndex(index) < this.size);
-    }
-    // Returns boolean for Parent
-    hasParent(index) {
-        return (this.storage[this.storage.getParentIndex(index)]);
+        delItem = this.storage.pop();
+        this.siftDown(0);
     }
     // Returns the maximum value in the heap in constant time
     getMax() {
-        this.storage.filter(value => { 
-            return ((value > this.storage[0]) ? value : storage[0]);
-        });
-        return this;
+        this.storage[0] = this.storage.pop()
+      return ;
     }
     // Returns the size of the heap
     getSize() {
-        this.size = this.storage.length();
+        this.size = this.storage.length;
         return this.size;
     }
     // Returns the storage array
@@ -76,43 +46,36 @@ class Heap {
     bubbleUp(index) { 
         while (index > 0) {
             let parentNode = Math.floor((index +1) / 2) -1;
-            let parent =  this.storage[parentNode];
-            if (parent >= this.storage[index]) break;
-            this.storage[parentNode] = index;
-            this.storage[index] = parent;
+            if (this.storage[parentNode] > this.storage[index])
+            this.swap(parentNode, index);
             index = parentNode;
-        }
+        }  
     }
     
     // First grabs the indices of this element's children and determines which of the children are larger
     // If the larger of the child elements is larger than the parent, the child element is swapped with the parent
     // This method is only used by the heap itself in order to maintain the heap property
     siftDown(index) {
-      if (this.storage.length === 0) return undefined;
-      let storage = this.storage[0];
-      storage = this.storage[this.storage.length -1];
-      this.size--;
-      index = 0;
-      while (this.hasLeftChild(index) !== null) {
-          let smallerChildIndex = this.getLeftChildIndex(index);
-          if (this.hasRightChild(index) && this.hasrightChild(index) < this.leftChild(index)) {
-              smallerChildIndex = this.getRightChildIndex(index);
-          }
-          if (this.storage[index] < this.storage[smallerChildIndex]) {
-              break;
-          } else {
-              this.swap(index, smallerChildIndex);
-          }
-          index = smallerChildIndex;
-      }
-
+        while (true) {
+            let child = (index + 1) * 2;
+            let nextChild = child -1;
+            let swapThis = null;
+            if (this.storage[index] > this.storage[child]) swapThis = child;
+            if (this.storage[index] > this.storage[nextChild] && (this.storage[child] === null || (this.storage[child] !== null && this.storage[nextChild] < this.storage[child]))) {
+                swapThis = nextChild;
+            }
+            if (swapThis === null || this.storage[swapThis] == null) break;
+            this.swap(swapThis, index);
+            index = swapThis;
+            }     
     }
 }
-const arr = [10, 3, 4, 8, 2, 9, 7, 1, 2, 6, 5];
-const heap = new Heap(function(x){return x;});
-arr.forEach(item => heap.insert(item));
+// const arr = [10, 3, 4, 8, 2, 9, 7, 1, 2, 6, 5];
+// const heap = new Heap(function(x){return x;});
+// arr.forEach(item => heap.insert(item));
       
-heap.delete();
-while (heap.size() > 0)
-  print(heap.pop());
+// heap.delete();
+// while (heap.getSize() > 0)
+//   console.log(heap.getMax());
 module.exports = Heap;
+
