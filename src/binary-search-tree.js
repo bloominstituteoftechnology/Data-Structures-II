566ca024b11c4bf50d44c789dfaf78b60aa686c4
FJF616 <franzjfernando@hotmail.com>
2017-11-26 19:13:23 -0800

    fixing more spacing errors

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -62,16 +62,6 @@ class BinarySearchTree {
 
   */ 
   depthFirstForEach(cb) {
-    // cb((value) => {
-    //   value = this.value;
-    //   if (this.left) {
-    //     this.left.depthFirstForEach(cb);
-    //   }
-    //   if (this.right) {
-    //     this.right.depthFirstForEach(cb);
-    //   }
-    // });
-    // Ivans solution:
     cb(this.value);
     if (this.left !== null) this.left.depthFirstForEach(cb);
     if (this.right !== null) this.right.depthFirstForEach(cb);
@@ -83,22 +73,6 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-  //   const queue = new Queue();
-  //   queue.enqueue(this);
-  //   while (!queue.isEmpty()) {
-  //     const node = queue.dequeue();
-  //     if (cb) {
-  //       cb(node.value);
-  //     }
-  //     if (!node.left) {
-  //       queue.enqueue(node.left);
-  //     }
-  //     if (!node.right) {
-  //       queue.enqueue(node.right);
-  //     }
-  //   }
-  // }
- 
     const queue = [];
     queue.push(this);
     for (let i = 0; i < queue.length; i++) {
