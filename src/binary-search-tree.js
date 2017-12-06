2b631f2c9a4ef2a53f71bc75bfeec2f03703e8e6
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-12-05 17:56:48 -0800

    add and finish with basic stuff and extra stuff except for heap data structure

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,38 +1,120 @@
-// https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
-/* eslint-disable global-require */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
-    this.left = null;
-    this.right = null;
+    this.left;
+    this.right;
   }
-  // Wraps the input value in a new BinarySearchTree and
-  // assigns it to either the left or right subtree,
-  // depending on its value
-  insert(value) {
 
-  }
-  // Checks the binary search tree for the input target
-  // Can be written recursively or iteratively
-  contains(target) {
+  insert(input) {
+    if(input < this.value){
+      if(!this.left) {
+        this.left = new BinarySearchTree(input);
+      } else {
+        this.left.insert(input);
+      }
+    } else if(input > this.value){
+      if (!this.right) {
+        this.right = new BinarySearchTree(input);
+      } else {
+        this.right.insert(input);
+      }
+    }
+  };
 
-  }
-  // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
-  depthFirstForEach(cb) {
+  contains(input) {
+    var result = false;
 
-  }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
-  // at the root node, going down to the root node's children, and iterating
-  // through all those nodes first before moving on to the next layer of nodes
-  // Applies the given callback to each tree node in the process
-  // You'll need the queue-helper file for this. Or could you roll your own queue
-  // again. Whatever floats your boat.
+    var inclusionCheck = function (input) {
+      if(this === undefined || this.value === undefined) {
+        result = false;
+      } else if(input === this.value){
+        result = true;
+      } else if(input < this.value){
+        inclusionCheck.call(this.left, input);
+      } else if(input > this.value){
+        inclusionCheck.call(this.right, input);
+      }
+    };
+
+    inclusionCheck.call(this, input);
+    return result;
+  };
+
+  depthFirstForEach(input) {
+    if(this.value){
+      input(this.value);
+    }
+    if(this.left){
+      this.left.depthFirstForEach(input);
+    }
+
+    if(this.right){
+      this.right.depthFirstForEach(input);
+    }
+  };
+
+  depthFirstToArray(input, vals = []) {
+    if(this.value){
+      vals.push(this.value);
+    }
+    if(this.left){
+      this.left.depthFirstToArray(input, vals);
+    }
+
+    if(this.right){
+      this.right.depthFirstToArray(input, vals);
+    }
+    return vals;
+  };
+
   breadthFirstForEach(cb) {
+    var current = [this];
 
-  }
-}
+    while (current.length > 0) {
+
+      var next = [];
+
+      for (var node of current) {
+
+        cb(node.value);
+
+        if (node.left) {
+          next.push(node.left);
+        }
+        if (node.right) {
+          next.push(node.right);
+        }
+
+      current = next;
+      }
+    }
+  };
+
+  breadthFirstToArray(vals = []) {
+    let current = [this];
+
+    while(current.length > 0){
+
+      let next = [];
+
+      for(var node of current) {
+        vals.push(node.value);
+
+        if(node.left){
+          next.push(node.left);
+        }
+
+        if(node.right){
+          next.push(node.right);
+        }
+
+      }
+
+      current = next;
+    }
+
+    return vals;
+  };
+};
 
 module.exports = BinarySearchTree;
