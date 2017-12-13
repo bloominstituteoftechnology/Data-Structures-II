e0b04b04779b82fe5f91fddd6bb3ef35f489f9bb
Samuel Cha <sunchipssam@gmail.com>
2017-12-13 15:54:39 -0800

    redid breadthFirstForEach with queue and passed all tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -19,7 +19,7 @@ class BinarySearchTree {
         this.left = new BinarySearchTree(value);
       }
     } else if (this.right) {
-      this.right.insert(value);      
+      this.right.insert(value);
     } else {
       this.right = new BinarySearchTree(value);
     }
@@ -42,35 +42,19 @@ class BinarySearchTree {
       this.left.depthFirstForEach(cb);
     }
     if (this.right) {
-      this.right.depthFirstForEach(cb);      
+      this.right.depthFirstForEach(cb);
     }
     return;
   }
 
   breadthFirstForEach(cb) {
     const queue = new Queue();
-
-    let layerHasChildren = true;
-    let parentArr = [this];
-    let childrenArr = [];
-    while (layerHasChildren) {
-      // we do not know if the new layer has children yet
-      layerHasChildren = false;
-      // for each parent (node) in the layer
-      for (let i = 0; i < parentArr.length; i++) {
-        const parent = parentArr[i];
-        cb(parent.value);
-        if (parent.left !== null) {
-          layerHasChildren = true;
-          childrenArr.push(parent.left);
-        }
-        if (parent.right !== null) {
-          layerHasChildren = true;
-          childrenArr.push(parent.right);
-        }
-      }
-      if (layerHasChildren) parentArr = childrenArr.slice();
-      childrenArr = [];
+    queue.enqueue(this);
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      cb(node.value);
+      if (node.left) queue.enqueue(node.left);
+      if (node.right) queue.enqueue(node.right);
     }
   }
 }
