141b7899a86ff370e0567a5b3765e2520edccbbf
John C <johnjohnx4@gmail.com>
2017-12-13 17:41:15 -0500

    Finished trees.js and binary-search-trees all tests passing

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,30 +8,42 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
-  // Wraps the input value in a new BinarySearchTree and
-  // assigns it to either the left or right subtree,
-  // depending on its value
+
   insert(value) {
-
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new BinarySearchTree(value);
+      }
+      this.left.insert(value);
+    } else if (value > this.value) {
+      if (!this.right) { 
+        this.right = new BinarySearchTree(value); 
+      }
+      this.right.insert(value);
+    }
   }
-  // Checks the binary search tree for the input target
-  // Can be written recursively or iteratively
+
   contains(target) {
-
+    if (target === this.value) return true;
+    else if (this.left === null || this.right === null) return false;
+    else if (target < this.value) return this.left.contains(target);
+    else if (target > this.value) return this.right.contains(target);
   }
-  // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
+
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if (this.left !== null) this.left.depthFirstForEach(cb);
+    if (this.right !== null) this.right.depthFirstForEach(cb);
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
-  // at the root node, going down to the root node's children, and iterating
-  // through all those nodes first before moving on to the next layer of nodes
-  // Applies the given callback to each tree node in the process
-  // You'll need the queue-helper file for this. Or could you roll your own queue
-  // again. Whatever floats your boat.
-  breadthFirstForEach(cb) {
 
+  breadthFirstForEach(cb) {
+    const queue = [];
+    queue.push(this);
+    for (let i = 0; i < queue.length; i++) {
+      cb(queue[i].value);
+      if (queue[i].left) queue.push(queue[i].left);
+      if (queue[i].right) queue.push(queue[i].right);
+    }
   }
 }
 
