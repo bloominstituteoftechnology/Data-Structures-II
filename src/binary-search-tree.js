6dfef5f1d9f89b4278ddd8a92be603433ca6e16b
Samuel Cha <sunchipssam@gmail.com>
2017-12-14 15:40:29 -0800

    readded return statements to be thorough for non-test cases

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -18,6 +18,7 @@ class BinarySearchTree {
     }
     if (value < this.value && this.left !== null) {
       this.left.insert(value);
+      return;
     }
     if (value >= this.value && this.right === null) {
       this.right = new BinarySearchTree(value);
@@ -25,6 +26,7 @@ class BinarySearchTree {
     }
     if (value >= this.value && this.right !== null) {
       this.right.insert(value);
+      return;
     }
   }
 
