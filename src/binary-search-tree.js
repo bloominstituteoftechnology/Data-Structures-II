7f553d53ced245ca39e2ea3854408f78fc209ef6
Samuel Cha <sunchipssam@gmail.com>
2017-12-14 09:32:53 -0800

    made insert and contains use explicit checking !== null as well

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -13,12 +13,12 @@ class BinarySearchTree {
 
   insert(value) {
     if (value <= this.value) {
-      if (this.left) {
+      if (this.left !== null) {
         this.left.insert(value);
       } else {
         this.left = new BinarySearchTree(value);
       }
-    } else if (this.right) {
+    } else if (this.right !== null) {
       this.right.insert(value);
     } else {
       this.right = new BinarySearchTree(value);
@@ -27,10 +27,10 @@ class BinarySearchTree {
 
   contains(target) {
     if (this.value === target) return true;
-    if (this.left) {
+    if (this.left !== null) {
       if (this.left.contains(target)) return true;
     }
-    if (this.right) {
+    if (this.right !== null) {
       if (this.right.contains(target)) return true;
     }
     return false;
@@ -38,8 +38,8 @@ class BinarySearchTree {
 
   depthFirstForEach(cb) {
     cb(this.value);
-    if (this.left) this.left.depthFirstForEach(cb);
-    if (this.right) this.right.depthFirstForEach(cb);
+    if (this.left !== null) this.left.depthFirstForEach(cb);
+    if (this.right !== null) this.right.depthFirstForEach(cb);
     return;
   }
 
