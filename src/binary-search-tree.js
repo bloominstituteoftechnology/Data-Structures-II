d360f2892ce9d94ed3e27d15bc501131cbd90a5e
Guelmis Cortina <cguelmis@yahoo.com>
2017-12-14 10:31:59 -0500

    finished tree.js file. Done with insert and contains in the binary-search-tree.js file

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,19 +12,43 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const newNode = new BinarySearchTree(value);
+    const assing = (rootNode) => {
+      if (rootNode.value >= value && rootNode.left === null) {
+        rootNode.left = newNode;
+        return;
+      }
+      if (rootNode.value >= value) assing(rootNode.left);
+      if (rootNode.value <= value && rootNode.right === null) {
+        rootNode.right = newNode;
+        return;
+      }
+      if (rootNode.value <= value) assing(rootNode.right)
+    };
+    assing(this);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let foundNode = false;
+    const finder = (rootNode) => {
+      if (rootNode.value === target) {
+        foundNode = true;
+      } else if (rootNode.left !== null && target < rootNode.value) {
+        finder(rootNode.left);
+      } else if (rootNode.right !== null && target > rootNode.value) {
+        finder(rootNode.right);
+      }
+    };
+    finder(this);
+    return foundNode;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
 
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
+  // Traverses the tree in a breadth-first manner, i.e. in layers, starting
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
