2d43bc57df93b5fdf0b81a62ad2cf652d657b22f
Clara Andrew <claraandrew@Claras-MacBook-Pro.local>
2017-12-14 13:07:56 -0500

    binary search tree test passed

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,17 +12,29 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const newNode = new BinarySearchTree(value);
+    if (this.value > value && !this.left) this.left = newNode;
+    else if (this.value > value) this.left.insert(value);
+    else if (value > this.value && !this.right) this.right = newNode;
+    else if (value > this.value) this.right.insert(value);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) return true;
+    else if (this.value > target && !this.left) return false; 
+    else if (target > this.value && !this.right) return false;
+    else if (this.value > target) return this.left.contains(target);        
+    else if (target > this.value) return this.right.contains(target);   
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    const arr = [];
+    if (this.value) arr.push(cb(this.value));
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
+    return arr;
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,7 +43,22 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = [];
+    queue.push(this);
+    for (let i = 0; i < queue.length; i++) {
+      cb(queue[i].value);
+      if (queue[i].left) queue.push(queue[i].left);
+      if (queue[i].right) queue.push(queue[i].right);
+    }
+    // if (this.value) arr.push(cb(this.value));
+    // if (this.left && this.right) {
+    //   this.left.breadthFirstForEach(cb);
+    //   this.right.breadthFirstForEach(cb);
+    //   return
+    // }
+    // if (this.left) return this.left.breadthFirstForEach(cb);
+    // if (this.right) return this.right.breadthFirstForEach(cb);
+    // return arr;
   }
 }
 
