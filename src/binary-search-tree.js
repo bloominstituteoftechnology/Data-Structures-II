7fa77e63cc468d80a0946dbe4e09c8d1345a44d0
unknown <brcampbell001@outlook.com>
2017-12-14 20:57:13 -0600

    Ben Campbell DS II

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,30 +8,55 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
-  // Wraps the input value in a new BinarySearchTree and
-  // assigns it to either the left or right subtree,
-  // depending on its value
+  
   insert(value) {
-
+    const node = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = node;
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = node;
+      } else {
+        this.right.insert(value);
+      }
+    }
   }
-  // Checks the binary search tree for the input target
-  // Can be written recursively or iteratively
+
   contains(target) {
 
+    if (this.value === target) return true;
+    if (target < this.value) {
+      if (this.left === null) return false;
+      return this.left.contains(target);
+    } else if (target > this.value) {
+      if (this.right === null) return false;
+      return this.right.contains(target);
+
+    }
   }
-  // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
+
   depthFirstForEach(cb) {
 
+    cb(this.value);
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
+
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
-  // at the root node, going down to the root node's children, and iterating
-  // through all those nodes first before moving on to the next layer of nodes
-  // Applies the given callback to each tree node in the process
-  // You'll need the queue-helper file for this. Or could you roll your own queue
-  // again. Whatever floats your boat.
+
   breadthFirstForEach(cb) {
 
+    const Queue = [];
+    Queue.push(this);
+    for (let i = 0; i < Queue.length; i++) {
+      cb(Queue[i].value);
+      if (Queue[i].left) Queue.push(Queue[i].left);
+      if (Queue[i].right) Queue.push(Queue[i].right);
+
+    }
   }
 }
 
--- src/tree.js
+++ src/tree.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
 class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
   }
-  // Adds a new Tree node with the input value to the current Tree node 
+
   addChild(value) {
 
+    const childrensTree = new Tree(value);
+    this.children.push(childrensTree);
   }
-  // Checks this node's children to see if any of them matches the given value
-  // Continues recursively until the value has been found or all of the children
-  // have been checked
-  contains(value) {
 
+  contains(value) {
+    
+    if (this.value === value) return true;
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+      if (child.value === value) return true;
+      if (child.children[0] && child.contains(value)) return true;
+    }
+    return false;
   }
 }
 
