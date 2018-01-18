13a869ab2a7f0c1b63983cd4599206713ae948c8
Aaron <aarondburk@gmail.com>
2018-01-18 16:08:58 -0600

    what I was able to accomplish before the teacher showed us how to do it

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,7 +1,7 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
+/* eslint-disable */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,12 +12,47 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    // insert needs to instantiate a new node
+    const newNode = new BinarySearchTree(value);
+    // insert needs to check if the root node is equal
+    // or less than the value passed into it.
+    if (value <= this.value) {
+      // now it inserts the value to the left
+      this.left = new BinarySearchTree(value);
+    }
+    // or since it was not less than or equl to the root
+    // node then it has to insert it to the right.
+    this.right = new BinarySearchTree(value);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let findsTarget = false;
+    if (target === this.value) return findsTarget = true;
+    if (target < this.value) {
+      const search = (left) => {
+        left.forEach((leftValue) => {
+          if (target === this.value) return findsTarget = true;
+          if (target < left.value) {
+            search(leftValue.left);
+          }
+        });
+      };
+      search(this.right);
+      return findsTarget;
+    }
+    if (target > this.value) {
+      const search = (right) => {
+        right.forEach((rightValue) => {
+          if (target === this.value) return findsTarget = true;
+          if (target < right.value) {
+            search(rightValue.right);
+          }
+        });
+      };
+      search(this.right);
+      return findsTarget;
+    }
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
