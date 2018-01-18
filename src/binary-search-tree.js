be4b39d916f339bb6d9936d83089ec223edc3621
Smithface <kylewibblesmith@gmail.com>
2018-01-18 16:28:41 -0500

    almost understand trees, starting to work through binary-search-tree; just passed insert test

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,7 +1,6 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,26 +11,43 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const blah = this.left + 3;
+    const bsTree = new BinarySearchTree(value);
+    const recur = (side, val) => {
+      if (side.value < val) {
+        if (side.right === null) {
+          side.right = bsTree;
+        } else {
+          recur(side.right, val);
+        }
+      } else if (side.value > val) {
+        if (side.left === null) {
+          side.left = bsTree;
+        } else {
+          recur(side.left, val);
+        }
+      }
+    };
+    recur(this, value);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    const bslah = this.left + 3;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    const bladh = this.left + 3;
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
+  // Traverses the tree in a breadth-first manner, i.e. in layers, starting
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const blgah = this.left + 3;
   }
 }
 
--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable no-trailing-spaces */
+/* eslint-disable */
 // Do not modify this GraphNode class
 // Use any of its methods as you see fit to implement your graph
 class GraphNode {
@@ -35,7 +35,7 @@ class Graph {
     this.vertices = [];
   }
   // Wraps the input value in a new GraphNode and adds it to the array of vertices
-  // If there are only two nodes in the graph, they need to be automatically 
+  // If there are only two nodes in the graph, they need to be automatically
   // connected via an edge
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
@@ -47,7 +47,7 @@ class Graph {
   contains(value) {
 
   }
-  // Checks the graph to see if a GraphNode with the specified value exists in the graph 
+  // Checks the graph to see if a GraphNode with the specified value exists in the graph
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
@@ -56,13 +56,13 @@ class Graph {
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
   // If only one vertex references the other but not vice versa, should not return true
-  // Note: You'll need to store references to each vertex's array of edges so that you can use 
+  // Note: You'll need to store references to each vertex's array of edges so that you can use
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
 
   }
   // Adds an edge between the two given vertices if no edge already exists between them
-  // Again, an edge means both vertices reference the other 
+  // Again, an edge means both vertices reference the other
   addEdge(fromVertex, toVertex) {
 
   }
