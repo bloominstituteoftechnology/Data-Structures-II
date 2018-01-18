f00489350ff9ab4e9b689f4569c1c9da08bfe75c
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-01-18 16:22:09 -0500

    Completed insert and contains

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,22 +2,44 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.root = null;
   }
   // Wraps the input value in a new BinarySearchTree and
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value <= this.value) { // if input value is less than or equal to root node value
+      if (!this.left) { // check if left child exists
+        this.left = new BinarySearchTree(value); // if left child does not exist, assign new BST to that location
+      }
+      else this.left.insert(value); // if left child already exists, call insert again on the left child node
+    }
+    if (value > this.value) { // if input value is greater than root node value
+      if (!this.right) { // check if right child exists
+        this.right = new BinarySearchTree(value); // if right child does not exist, assign new BST to that location
+      }
+      else this.right.insert(value); // if right child already exists, call insert again on the right child node
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    let containsValue = false; // initialize flag
+    if (this.value === target) return containsValue = true; // if current node value equals target, return true
+    if (target < this.value) { // if target value is less than current value
+      if (!this.left) return containsValue; // check if left child exists. if not, return false
+      return this.left.contains(target); // if left child exists, call contains on this node
+    }
+    if (target > this.value) { // if target value is greater than current node value
+      if (!this.right) return containsValue; // check if right child node exists. if not, return false
+      return this.right.contains(target); // if right child exists, call contains on this node
+    }
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
