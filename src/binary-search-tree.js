2db38b1eb3b85f492fc410e3c3b5763071f5c8bc
cliffkang <cliffgkang@gmail.com>
2018-01-18 15:17:03 -0800

    finished insert for binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,27 +12,54 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const search = (newNode) => {
+      const newBinaryTree = new BinarySearchTree(value);
+      if (value >= newNode.value) {
+        if (!newNode.right) newNode.right = newBinaryTree;
+        else search(newNode.right);
+      } else {
+        if (!newNode.left) {
+          newNode.left = newBinaryTree;
+          return;
+        }
+        search(newNode.left);
+      }
+    };
+    search(this);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
+    
   contains(target) {
-
+    let containsValue = false;
+    if (target === this.value) return containsValue = true;
+    const search = (newNode) => {
+      if (target >= newNode.value) {
+        const rightSide = newNode.right;
+        if (target === rightSide) return containsValue = true;
+        if (rightSide !== null) search(rightSide);
+      } 
+      const leftSide = newNode.left;
+      if (target === leftSide) return containsValue = true;
+      if (leftSide !== null) search(leftSide);
+    };
+    search(this);
+    return containsValue;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
-  depthFirstForEach(cb) {
+  // depthFirstForEach(cb) {
 
-  }
+  // }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
-  breadthFirstForEach(cb) {
+  // breadthFirstForEach(cb) {
 
-  }
+  // }
 }
 
 module.exports = BinarySearchTree;
