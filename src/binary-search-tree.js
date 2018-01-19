4571c4ad03797a5ecf589350c4af3b3b7788d9e6
aceredesign <steven.magadan@hotmail.com>
2018-01-19 13:48:32 -0800

    error in graph

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,26 +12,48 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value <= this.value) {
+      if (!this.left) this.left = new BinarySearchTree(value);
+      else this.left.insert(value);
+    } else if (value > this.value) {
+      if (!this.right) this.right = new BinarySearchTree(value);
+      else this.right.insert(value);
+    }
   }
-  // Checks the binary search tree for the input target
+  // Checks the binary search tree for the input value
   // Can be written recursively or iteratively
-  contains(target) {
-
+  contains(value) {
+    if (this.value === value) return true;
+    if (value < this.value) {
+      if (!this.left) return false;
+      return this.left.contains(value);
+    } else if (value > this.value) {
+      if (!this.right) return false;
+      return this.right.contains(value);
+    }
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
-  depthFirstForEach(cb) {
 
+  depthFirstForEach(cb) {
+    cb(this.value);
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
+  // Traverses the tree in a breadth-first manner, i.e. in layers, starting
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = [this];
+    while (queue.length) {
+      const treeNode = queue.shift();
+      cb(treeNode);
+      if (treeNode.left) queue.push(treeNode.left);
+      if (treeNode.right) queue.push(treeNode.right);
+    }
   }
 }
 
