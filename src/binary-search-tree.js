764635d29db0517f5ce48f7ed3a8d098081dd340
Smithface <kylewibblesmith@gmail.com>
2018-01-19 01:24:35 -0500

    finished tree and binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,8 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
+const Queue = require('./queue-helper');
+
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -11,34 +13,62 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-    const blah = this.left + 3;
+    // assign something using 'this' keyword to make linter happy
+    const recurOnThis = this;
+    // create a new tree, because the value has to go somewhere
     const bsTree = new BinarySearchTree(value);
+    // use recursion to check if our value is > or < the value of each node we check
+    // assign value only when we find a side === null
     const recur = (side, val) => {
       if (side.value < val) {
         if (side.right === null) {
-          side.right = bsTree;
-        } else {
-          recur(side.right, val);
+          return side.right = bsTree;
         }
+        recur(side.right, val);
       } else if (side.value > val) {
         if (side.left === null) {
-          side.left = bsTree;
-        } else {
-          recur(side.left, val);
+          return side.left = bsTree;
         }
+        recur(side.left, val);
       }
     };
-    recur(this, value);
+    // call the recursion function
+    recur(recurOnThis, value);
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-    const bslah = this.left + 3;
+    const recurOnThis = this;
+    let result = false;
+    // use the same recursion as before, but with an extra check for the given target value
+    // return false if we need to move to a node that does not exist
+    const recur = (side, val) => {
+      if (side.value === val) return result = true;
+      if (side.value < val) {
+        if (side.right === null) {
+          return result = false;
+        }
+        recur(side.right, val);
+      } else if (side.value > val) {
+        if (side.left === null) {
+          return result = false;
+        }
+        recur(side.left, val);
+      }
+    };
+    recur(recurOnThis, target);
+    return result;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-    const bladh = this.left + 3;
+    const recurOnThis = this;
+    const recur = (side) => {
+      cb(side.value);
+      if (side.left) recur(side.left);
+      if (side.right) recur(side.right);
+    };
+    recur(recurOnThis);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting
   // at the root node, going down to the root node's children, and iterating
@@ -47,7 +77,14 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-    const blgah = this.left + 3;
+    const q = new Queue();
+    q.enqueue(this);
+    while (!q.isEmpty()) {
+      const currentTree = q.dequeue();
+      if (currentTree.left) q.enqueue(currentTree.left);
+      if (currentTree.right) q.enqueue(currentTree.right);
+      cb(currentTree.value);
+    }
   }
 }
 
