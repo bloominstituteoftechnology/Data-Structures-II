d6a974daf70ed139a7d9d19cda2bb807a6e474a1
Dani Tacheny <dani.tacheny@gmail.com>
2018-01-20 16:50:03 -0600

    passed tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,6 +2,7 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable */
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,17 +13,42 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    const search = (node) => {
+      if (node.value === target) return true;
+      if (node.value < target) {
+        if (node.right === null) return false;
+        return search(node.right);
+      }
+      if (node.value > target) {
+        if (node.left === null) return false;
+        return search(node.left);
+      }
+    }
+    return search(this);
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if (this.left !== null) this.left.depthFirstForEach(cb);
+    if (this.right !== null) this.right.depthFirstForEach(cb);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,7 +57,17 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = [this];
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+      cb(currentNode.value);
+    }
   }
 }
 
