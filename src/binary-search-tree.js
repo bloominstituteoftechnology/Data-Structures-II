5bb7a3a43f6906b494bf59340d6c824929b3c9a2
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-01-21 20:41:54 -0500

    Binary Search Tree and Tree Finished

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -3,9 +3,15 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+const Queue = require('./queue-helper');
+
 class BinarySearchTree {
   constructor(value) {
+    // Pass in a value for the Tree/Tree node into (using Quakka example) the "shoe box labelled myBinary.value"
     this.value = value;
+    // Set its left child and right child to null
+    // From FunFunFunction Recursion -- this just means "does not have any sub-categories"
+    // How is null different from []???
     this.left = null;
     this.right = null;
   }
@@ -13,27 +19,104 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    // ??? I know you have to use the 'new' keyword when creating instances, but why do you need to use it
+    // when creating a new binary search tree? Is each of these new nodes basically a new instance of the
+    // BinarySearchTree class???
+    const newNode = new BinarySearchTree(value);
+    // ??? If I wanted to assign a value to the Tree node, how woudl I do that? ???
+    // So is this saying: IF 'value' is less than the Tree node, THEN...
+    // if(!value){
+    // this.value = new Node(val)
+    // return;
+    if (value < this.value) {
+      // if this.left (the next left node) is not yet defined(aka the space there is available), then newNode becomes this.left
+      if (!this.left) {
+        this.left = newNode;
+      } else {
+        // Read this SLOWLY to understand what is happening withe dot notation below:
+        // https://www.codecademy.com/en/forum_questions/5170307264a7402d9a0012f5
+        // otherwise [if this.left DOES exist - this is implied because bang operator ! is used], for example, if the Tree node
+        // is 10, and this.left is occupied by the value 9, then this.left.insert(value) tells the computer to make that node
+        // the 'value' in the insert function.
+        // For example if you are looking to enter the value 8, and 'this.value' - the Tree node is 10, then the first IF
+        // statement is true (8 < 10).  Lets say the node left of the Tree node is 9.  Then this.left is defined, and 9 becomes
+        // the 'value' in insert(value).  So again, (think of the definition of binary trees) 9 is compared with this.value
+        // which equals 10.  Since 9 is less than 10, the next IF statement checks if 9 has a this.left.  It does!  this.left of 9
+        // is 
+        // To us insert(value) on a specific array of values (this.left)
+        this.left.insert(value);
+      }
+      // implied -> If the value is equal to or greater than this.value, then proceed here.
+    } else {
+      if (!this.right) {
+        this.right = newNode;
+      } else {
+        this.right.insert(value); // Using dot notation, chaining the function.  this.right is the array 
+      }
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) {
+      return true;
+    }
+    // if this.value has a defined this.left
+    if (this.left) {
+      // then for this.left....ACK! Same notation from earlier!  Don't know how it works???????????
+      if (this.left.contains(target)) return true;
+    }
+    if (this.right) {
+      if (this.right.contains(target)) return true;
+    }
+    return false;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
-  depthFirstForEach(cb) {
-
+  depthFirstForEach(cb) { // Common Interview topic
+    // Passing the Tree node into the cb function means that each node in the entire tree is, one by one, starting with the left
+    // branch
+    cb(this.value);
+    // traverse down the left side completely first, until there are no more this.left nodes (aka we reach the leaf node)
+    if (this.left) {
+      this.left.depthFirstForEach(cb);
+    }
+    // As soon as this.left comes up undefined, then you traverse down the right side.  Each time you go through
+    // through depthFirstForEach, the computer will see that...??? and this is where I get lost.
+    if (this.right) {
+      this.right.depthFirstForEach(cb);
+    }
   }
+
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
-  breadthFirstForEach(cb) {
-
+  breadthFirstForEach(cb) { // Common Interview Question 
+    // Declares a new instance of the Queue class called 'queue'
+    const queue = new Queue();
+    // Pushes 'this'???cb? onto the this.storage array
+    queue.enqueue(this);
+    // Assuming that the tree is not empty,
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      // if a left node exists
+      if (node.left) {
+        // push 'node.left'??? onto the this.storage array
+        queue.enqueue(node.left);
+      }
+      // if the right node exists
+      if (node.right) {
+        // push 'node.right'??? onto the this.storage array
+        queue.enqueue(node.right);
+      }
+      cb(node.value);
+    }
   }
 }
+
+
 
 module.exports = BinarySearchTree;
--- src/tree.js
+++ src/tree.js
@@ -1,32 +1,61 @@
-
+/* eslint-disable */
 class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
   }
+  // Adds a new Tree node with the input value to the current Tree node
+  //  // Whatever node is input, that node becomes the top of the tree aka the Tree Node
+  //  // A tree node is able to access its  
   addChild(value) {
-    const tree = new Tree(value);
-    this.children.push(tree);
+      // Create a new instance 'tree', which passes the input 'value' into the Tree parent class
+      const tree = new Tree(value);
+      // the this context still refers to initial tree, so you can write the following in order to
+      // push the 
+      // Same as 'this.children.push(new Tree(value)); if you don't like the 'tree' variable
+      // 'tree' aka the new Tree node, is then pushed to the children array.
+      this.children.push(tree);  
+
   }
+  // Checks this node's children to see if any of them matches the given value
+  // Continues recursively until the value has been found or all of the children
+  // have been checked
+  //  // Now we look through each of the nodes in the Tree's children array - called 'child'
+  //  // "We are going to start at a node and we are going to check all of its children to see if any of that tree
+  //  // node's children match the value we are looking for." (depthFirstForEach)
+  //  // depthFirst -- Down the side of left side of the tree first
+  //  //            -- Then back up searching recursively through each of the children nodes spanning to the right
+  //  //            -- Until the top is reached, at which point the search goes down the right side first
+  //  //            -- Then checks each of the children nodes in each 
   contains(value) {
-    let containsValue = false;
-    if (this.value === value) 
-    containsValue = true;
-    return
-    if (this.value !== value) {
-      const search = (children) => {
-        children.forEach((child) => {
-          if (value === child.value)
-          containsValue = true;
-          return
-          if (child.children.length) {
-            search(child.children);
-          }
-        });
+      // start off with a flag variable and set it to false; assumes false until 'value' is found
+      let containsValue = false;
+      // if the 'this.value' - the "Tree node" at the top of the tree ('Dr.Appleton' in Quakka example) is equal
+      // to the value you are searching for, then you have found the value!  Return true!
+      if (this.value === value) return containsValue = true;
+      // If 'Dr.Appleton' is not the value you are searching for, then you will need to tell the computer to search
+      // recursively through the entire family tree. -- NONONO!  This is wrong! The above if statement is entirely separate
+      // from what is below.  First the above if statement runs, and if the Tree node is the value being searched for, then
+      // the program closes.  
+      // The code below is NOT part of the if statement!  The code below deals with the entirely separate scenario, which
+      // could be written inside of --if (this.value !== value) {HERE}-- but this line is not written in because it is 
+      // unnecessary to do so.
+        const search = (children) => {
+          // For each of Dr.Appleton's children, first they will be compared with the child('value') you are seeking, then
+          // then IF they have children, then 
+          children.forEach((child) => {
+              if (value === child.value) return containsValue = true;
+              // checks to see if there are more children to look through
+              // Recursion - maps out the entire Tree first,
+              if (child.children.length); {
+                  search(child.children);
+              }
+          });
       };
+      // This is the point at which search is actually executed.  The above is all just the recursive function 'search'
+      // being defined.  
       search(this.children);
       return containsValue;
-    }
   }
 }
 
