6ccef523ae97a836b832bd8903094524768d5bbc
itsdevinci <devincitechsolutions@gmail.com>
2018-01-22 01:57:26 -0800

    Data Structures 2

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,24 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    let containsValue = false;
+    if (this.value === value) return containsValue = true;
+    const search = (children) => {
+      children.forEach((child) => {
+        if (value === child.value) return containsValue = true;
+        if (child.children.length) {
+          search(child.children);
+        }
+      });
+    };
+    search(this.children);
+    return containsValue;
   }
 }
 
