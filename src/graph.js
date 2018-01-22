a6a26e920a6330e4f9a37c7bda0dc9e807cbf4e1
algorithmsrock <algorithmsrock@gmail.com>
2018-01-21 23:46:16 -0800

    Data-Structures-II HW Almost Complete except for red-black trees and graphs (BFS and DFS)

--- src/graph.js
+++ src/graph.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-trailing-spaces */
@@ -40,40 +41,85 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-
+    const newNode = new GraphNode ({
+				value,
+				edges,
+	 });
+     
+		 // ensure that the edges that this node is connected to also 
+    //  connect back to this new node we're creating
+		if (edges.length > 0) {
+			edges.forEach((edge) => {
+					this.addEdge(newNode, edge);
+			});
+	  }
+		 this.vertices.push(newNode);
+		// we need to check if there are exactly two nodes in the graph and 
+	 //	 they do not have a connection between them
+    if (this.vertices.length == 2) {
+			this.addEdge(this.vertices[0], this.vertices[1]);
   }
-  // Checks all the vertices of the graph for the target value
+		return newNode;
+	}
+  
+	// Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+      let hasValue = false;
+			this.vertices.forEach((node) => {
+					if (node.value === value) {
+	 				  hasValue = true;
+						return;
   }
+	});
+			return hasValue;
+}
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-
-  }
+    const index = this.vertices.findIndex((node) => {
+				return node.value === value;
+	 });
+		if (index === -1) 
+			return;
+		const removedVertex = this.vertices.splice(index, 1)[0];
+		   removedVertex.edges.forEach((node) => {
+		   this.removeEdge(removedVertex, node);
+  });
+}
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
   // If only one vertex references the other but not vice versa, should not return true
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+     return fromVertex.edges.some(edge => edge === toVertex) && 
+			 toVertex.edges.some(edge => edge === fromVertex);
   }
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
-  }
+     if (!this.checkIfEdgeExists(fromVertex, toVertex)) {
+			 fromVertex.pushToEdges(toVertex);
+		   toVertex.pushToEdges(fromVertex);
+			 fromVertex.edges = fromVertex.edges.filter((edge, index) =>
+			fromVertex.edges.indexOf(edge) === index);
+			 toVertex.edges = toVertex.edges.filter((edge, index) => toVertex.edges.indexOf(edge) === index);
+     }
+	}
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   // If a vertex would be left without any edges as a result of calling this function, those
   // vertices should be removed as well
   removeEdge(fromVertex, toVertex) {
-
-  }
+    if (this.checkIfEdgeExists(fromVertex, toVertex)) {
+			fromVertex.edges = fromVertex.edges.filter(edge => edge.value !== toVertex.value);
+			toVertex.edges = toVertex.edges.filter(edge => edge.value !== fromVertex.value); 
+			if (fromVertex.numberOfEdges === 0) this.removeVertex(fromVertex.value);
+			if (toVertex.numberOfEdges === 0) this.removeVertex(toVertex.value);
+		}
+	}
 }
-
 module.exports = Graph;
 
