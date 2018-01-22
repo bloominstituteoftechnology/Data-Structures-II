6b46be769130fd39f4ed8c32264b326df56dc92c
IchabodPain <russell.l.bates@gmail.com>
2018-01-22 08:53:55 -0800

    Resubmitting, I had the same Git problem I think some others were dealing with. All complete except Heap.

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,7 +1,9 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
+/* eslint-disable */
+const { LimitedArray, getIndexBelowMax } = require('./queue-helper');
+
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,17 +14,31 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const newNode = new BinarySearchTree(value);
+    if (value >= this.value) {
+      if (this.right === null) this.right = newNode;
+      else this.right.insert(newNode.value);
+    }
+    else {
+      if (this.left === null) this.left = newNode;
+      else this.left.insert(newNode.value);
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
-  }
+      let containsValue = false;
+      if (target === this.value) return containsValue = true;
+      if (target < this.value && this.left) containsValue = this.left.contains(target);
+      if (target > this.value && this.right) containsValue = this.right.contains(target);
+      return containsValue;
+    }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    cb(this.value);
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,8 +47,26 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = new queue();
+    queue.enqueue(this);
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      if (node.left) {
+        queue.enqueue(node, left);
+      }
+      if (node.right) {
+        queue.enqueue(node, right);
+      }
+    cb(node.value);
+    }
   }
 }
+
+const primeNumbers = new BinarySearchTree(13);
+primeNumbers.insert(5);
+primeNumbers.insert(7);
+primeNumbers.insert(23);
+primeNumbers.insert(17);
+
 
 module.exports = BinarySearchTree;
