51e74fcd1682d25a2b7fb6e5d5be4df64acddd91
Alexander <alexander.botello@g.austincc.edu>
2018-01-29 22:02:13 -0600

    Implement a tree

--- src/tree.js
+++ src/tree.js
@@ -7,13 +7,22 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const node = new Tree(value);
+    this.children.push(node);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    let result = false;
+    const traverse = (node) => {
+      if (node.value === value) result = true;
+      for (let i = 0; i < node.children.length; i++) {
+        traverse(node.children[i]);
+      }
+    };
+    traverse(this);
+    return result;
   }
 }
 
