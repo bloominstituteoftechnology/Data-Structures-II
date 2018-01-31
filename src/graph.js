50ccce593d6f79d7d75a25a5334d1200b3dfc5b3
Andrew Jarrett <ahrjarrett@gmail.com>
2018-01-31 11:01:52 -0700

    graph: finish addVertex, contains & removeVertex

--- src/graph.js
+++ src/graph.js
@@ -41,39 +41,53 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-    const node = new GraphNode(value, edges);
-    this.vertices.push(node);
-    if (this.vertices.length === 2) {
-      this.vertices
+    const node = new GraphNode({ value, edges });
+    if (edges.length) {
+      node.pushToEdges(...edges);
     }
-    
+    this.vertices.push(node);
 
-
+    if (this.vertices.length === 2) {
+      this.vertices[0].pushToEdges(node.value)
+      this.vertices[1].pushToEdges(this.vertices[0].value)
+    }
   }
+
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    return this.vertices.some(v => v.value === value);
   }
+
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
+    // pulled off `removed` in case we wanted to go through the graph and remove all references to the removed vertex later
+    const [removed, index] = this.vertices.reduce((acc, v, idx) => {
+      if (v.value === value) return [v.value, idx]
+      return acc
+    }, [])
 
+    return this.vertices.splice(index)
   }
+
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
   // If only one vertex references the other but not vice versa, should not return true
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
+    
 
   }
+
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
 
   }
+
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   // If a vertex would be left without any edges as a result of calling this function, those
@@ -82,6 +96,13 @@ class Graph {
 
   }
 }
+
+const myGraph = new Graph()
+myGraph.addVertex('guido')
+myGraph.addVertex('bianca')
+myGraph.addVertex('grizzly', ['guido', 'bianca'])
+
+console.log(myGraph)
 
 module.exports = Graph;
 
