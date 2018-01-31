5651490a985d177d958dedf2426cf2557ae7142a
Alexander <alexander.botello@g.austincc.edu>
2018-01-30 18:59:11 -0600

    Implement a binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,3 +1,4 @@
+const Queue = require('./queue-helper');
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
@@ -12,17 +13,40 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    const node = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (!this.left) this.left = node;
+      else this.left.insert(value);
+      return;
+    }
+    if (!this.right) this.right = node;
+    else this.right.insert(value);
+    return;
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) return true;
+    else if (target < this.value) {
+      if (this.left) {
+        return this.left.contains(target);
+      }
+    } else if (target > this.value) {
+      if (this.right) {
+        return this.right.contains(target);
+      }
+    }
+    return false;
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    const traverse = (node) => {
+      cb(node.value);
+      if (node.left !== null) traverse(node.left);
+      if (node.right !== null) traverse(node.right);
+    };
+    traverse(this);
   }
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
@@ -31,7 +55,14 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const queue = new Queue();
+    queue.enqueue(this);
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      cb(node.value);
+      if (node.left) queue.enqueue(node.left);
+      if (node.right) queue.enqueue(node.right);
+    }
   }
 }
 
